1dd10cb02e36aa1d53e9a2619d9fbd327b3af32e
Tim Ermilov <yamalight@gmail.com>
2009-10-26 11:56:28 +0100

    [added] sitemap generator (not yet catches sitemap.xml request)

---
+++ extensions/components/semanticsitemap/SemanticsitemapController.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Semantic Sitemap plug-in controller
+ *
+ * @author Sebastian Dietzold <sebastian@dietzold.de>
+ * @copyright  Copyright (c) 2008, {@link http://aksw.org AKSW}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @version $Id: $
+ */
+class SemanticsitemapController extends OntoWiki_Controller_Component
+{
+    protected $_store = null;
+    protected $_models = null;
+    
+
+    /**
+    * inits the controller (e.g. for getting the config)
+    *
+    * @return void
+    */
+    public function init()
+    {
+        parent::init();
+
+        // Disable layout (we want only output xml)
+        $this->_helper->layout->disableLayout();
+
+        // Needed here because of the automatic rendering should be deactivated
+        //$this->_helper->viewRenderer->setNoRender(true);
+        
+        // Add the path of templates needed by this plugin
+        //$this->view->addScriptPath(REAL_BASE.'plugins/SemanticSitemap/templates/');
+        
+        // get the default store from the registry
+        $this->_store = Erfurt_App::getInstance()->getStore(); 
+
+        // fetch the modellist from the store
+        $this->_models = $this->_store->getAvailableModels();
+
+        // todo: where to set the content type? currently we get a Quirks mode :-(
+        $this->_response->setRawHeader('Content-Type: application/xml');
+    }
+
+
+    /**
+    * Generates a semantic sitemap according to Cyganiak et.al. (ESWC2008)
+    *
+    * @return void
+    */
+    public function sitemapAction()
+    {
+        $owApp = OntoWiki_Application::getInstance(); 
+        // these dataset items are used by the template
+        $datasets = array();
+        foreach ($this->_models as $modelUri => $model) {
+            $datasets[] = array(
+                'datasetURI' => $modelUri,
+                'datasetLabel' => $this->_store->getModel($modelUri)->getTitle(),
+                'sparqlEndpoint' =>  $owApp->config->urlBase . 'service/sparql',
+                'sparqlGraphName' => $modelUri,
+                'sparqlEndpointLocation' =>  $owApp->config->urlBase . 'service/sparql',
+                'dataDump' =>  $owApp->config->urlBase . 'model/export?output=xml&amp;m=' . urlencode($modelUri)
+            );
+        }
+
+        // assign view var(s)
+        $this->view->datasets = $datasets;
+        $this->view->appname = $version = $owApp->config->version->label;
+        $this->view->version = $version = $owApp->config->version->number;
+        $this->view->versionsuffix = $version = $owApp->config->version->suffix;
+
+
+        // render view
+        //$this->_response->setBody($this->view->render('default.php'));
+    }
+
+}
+
+?>
