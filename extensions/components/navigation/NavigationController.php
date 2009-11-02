3b9b025f163fca4663d83e3d6e4da933d1140cfe
Tim Ermilov <yamalight@gmail.com>
2009-11-02 15:25:14 +0000

    navigation controller

--- extensions/components/navigation/NavigationController.php
+++ extensions/components/navigation/NavigationController.php
@@ -60,7 +60,7 @@ class NavigationController extends OntoWiki_Controller_Component
         }
 
         $this->view->entries = $this->queryNavigationEntries($this->setup);
-        $this->view->setup = $this->setup;
+        //$this->view->setup = $this->setup;
         return;
     }
 
@@ -68,29 +68,140 @@ class NavigationController extends OntoWiki_Controller_Component
      * Queries all navigation entries according to a given setup
      */
     protected function queryNavigationEntries($setup) {
+        $handle = fopen('navi.log', "w");
+		    fwrite($handle, print_r($setup,true));     
+		    fclose($handle);
+        
         // used for generating internal OntoWiki Links
         $linkurl = new OntoWiki_Url(array('route' => 'properties'), array('r'));
 
-        $query = 'SELECT DISTINCT ?navEntry WHERE {
+        /*$query = 'SELECT ?navEntry WHERE {
             ?navEntry <'.EF_RDF_TYPE.'> <'.EF_OWL_CLASS.'>.
-            FILTER (isIRI(?navEntry)) .
-        }';
-        $results = $this->model->sparqlQuery ($query) ;
-
-        $titleHelper = new OntoWiki_Model_TitleHelper($this->model);
-        foreach ($results as $result) {
-            $titleHelper->addResource($result['navEntry']);
+            FILTER (isURI(?navEntry))
+            } LIMIT 10
+            ';*/
+        $query = $this->buildQuery($setup);
+        
+        // error logging
+    		$handle = fopen('navi.log', "a+");
+		    fwrite($handle, $query->__toString());     
+		    fclose($handle);
+        
+        $results = $this->model->sparqlQuery($query);
+    
+        $handle = fopen('navi.log', "a+");
+        fwrite($handle, "\n\n\n".print_r($results,true));     
+        fclose($handle);
+    
+        if( isset($setup->config->titleMode) ){ 
+            $mode = $setup->config->titleMode;
+        }else{ 
+            $mode = null;
         }
+        
+        $handle = fopen('navi.log', "a+");
+        fwrite($handle, "mode = ".$mode);     
+        fclose($handle);
+
+        if($mode !== null){
+            $titleHelper = new OntoWiki_Model_TitleHelper($this->model);
+            foreach ($results as $result) {
+                $titleHelper->addResource($result['navEntry']);
+            }
+        }
+        
+        $handle = fopen('navi.log', "a+");
+        fwrite($handle, "start res");     
+        fclose($handle);
+        
         $entries = array();
         foreach ($results as $result) {
             $uri = $result['navEntry'];
             $entry = array();
-            $entry['title'] = $titleHelper->getTitle($uri);
+            $entry['title'] = $this->getTitle($uri, $mode);
             $entry['link'] = (string) $linkurl->setParam('r', $uri, true);
             $entries[$uri] = $entry;
         }
 
         return $entries;
     }
-
+    
+    protected function getTitle($uri, $mode){
+        //if(!isset($mode) || $mode == null) 
+        $mode = "baseName";
+        if($mode == "titleHelper"){
+            return $titleHelper->getTitle($uri);
+        }else if($mode == "baseName"){
+            if(strrpos($uri, '/') > 0){
+                return substr($uri, strrpos($uri, '/')+1);
+            }else{
+                return $uri;
+            }
+        }
+    }
+    
+    protected function buildQuery($setup){
+        $query = new Erfurt_Sparql_SimpleQuery();
+        $prologue = 'SELECT DISTINCT ?navEntry ';// ?sub ?subsub ?subsubsub ';
+        $query->setProloguePart($prologue);
+        
+        $whereSpecs = array();
+        
+        // deeper qeury
+        if ( isset($setup->state->parent) && ($setup->state->lastEvent == "navigateDeeper") ) {
+            $whereSpec = '?navEntry a <'.$setup->state->parent.'>. ';
+            $whereSpec .= ' FILTER (isURI(?navEntry))';
+            $query->setWherePart('WHERE {' . $whereSpec . '}');
+            $query->setLimit(10);;        
+            return $query;
+        }
+        
+        // Init query
+        foreach ($setup->config->hierarchyTypes as $type) {
+            $whereSpecs[] = '{?navEntry a <' . $type . '>}';
+        }
+        
+        if ( !isset($setup->state->parent) ) {
+            foreach($setup->config->hierarchyRelations->in as $rel){
+                if ($rel == "http://www.w3.org/2000/01/rdf-schema#subClassOf") {
+                    $whereSpecs[] = '{?instance a ?navEntry.}';
+                }
+                // entities with a subtype must be a type
+                $whereSpecs[] = '{?subtype <' . $rel . '> ?navEntry.}';
+            }
+        }
+        
+        $whereSpec = implode(' UNION ', $whereSpecs);
+        $whereSpec .= ' FILTER (isURI(?navEntry))';
+        
+        // dont't show rdfs/owl entities and subtypes in the first level
+        if ( !isset($setup->state->parent) ) {
+            $whereSpec .= ' FILTER (regex(str(?super), "^' . EF_OWL_NS . '") || !bound(?super))';
+        }
+        foreach($setup->config->hierarchyRelations->in as $rel){
+            /*$whereSpec .= ' OPTIONAL {?sub <' . $rel . '> ?navEntry.
+                                OPTIONAL {?subsub <' . $rel . '> ?sub.
+                                      OPTIONAL {?subsubsub <' . $rel . '> ?subsub}
+                                }
+                            }';*/
+            $whereSpec .= ' OPTIONAL {?navEntry <' . $rel . '> ?super. FILTER(isUri(?super))}';
+        }
+        
+        // namespaces to be ignored, rdfs/owl-defined objects
+        if( isset($setup->config->hiddenNS) ){
+            foreach ($setup->config->hiddenNS as $ignore) {
+                $whereSpec .= ' FILTER (!regex(str(?navEntry), "^' . $ignore . '"))';
+            }
+        }
+        
+        // entry point into the class tree
+        if (isset($setup->state->parent)) {
+            $whereSpec .= ' FILTER (str(?super) = str(<' . $setup->state->parent . '>))';
+        }
+        
+        $query->setWherePart('WHERE {' . $whereSpec . '}');
+        
+        $query->setLimit(10);;        
+        return $query;
+    } 
 }
