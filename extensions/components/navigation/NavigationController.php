777a9b214fdf1a43bbe890fe7c030afe844a4f47
Tim Ermilov <yamalight@gmail.com>
2009-11-02 18:47:51 +0000

    nav controller

--- extensions/components/navigation/NavigationController.php
+++ extensions/components/navigation/NavigationController.php
@@ -18,6 +18,7 @@ class NavigationController extends OntoWiki_Controller_Component
     private $model;
     /* the setup consists of state and config */
     private $setup = null;
+    private $limit = 50;
 
     public function init()
     {
@@ -83,36 +84,18 @@ class NavigationController extends OntoWiki_Controller_Component
         $query = $this->buildQuery($setup);
         
         // error logging
-    		$handle = fopen('navi.log', "a+");
-		    fwrite($handle, $query->__toString());     
-		    fclose($handle);
+        $this->_owApp->logger->info($query->__toString());
         
         $results = $this->model->sparqlQuery($query);
     
-        $handle = fopen('navi.log', "a+");
-        fwrite($handle, "\n\n\n".print_r($results,true));     
-        fclose($handle);
+        // log results
+        $this->_owApp->logger->info("\n\n\n".print_r($results,true));     
     
         if( isset($setup->config->titleMode) ){ 
             $mode = $setup->config->titleMode;
         }else{ 
             $mode = null;
         }
-        
-        $handle = fopen('navi.log', "a+");
-        fwrite($handle, "mode = ".$mode);     
-        fclose($handle);
-
-        if($mode !== null){
-            $titleHelper = new OntoWiki_Model_TitleHelper($this->model);
-            foreach ($results as $result) {
-                $titleHelper->addResource($result['navEntry']);
-            }
-        }
-        
-        $handle = fopen('navi.log', "a+");
-        fwrite($handle, "start res");     
-        fclose($handle);
         
         $entries = array();
         foreach ($results as $result) {
@@ -127,9 +110,10 @@ class NavigationController extends OntoWiki_Controller_Component
     }
     
     protected function getTitle($uri, $mode){
-        //if(!isset($mode) || $mode == null) 
-        $mode = "baseName";
+        if(!isset($mode) || $mode == null) $mode = "baseName";
         if($mode == "titleHelper"){
+            $titleHelper = new OntoWiki_Model_TitleHelper($this->model);
+            $titleHelper->addResource($uri);
             return $titleHelper->getTitle($uri);
         }else if($mode == "baseName"){
             if(strrpos($uri, '/') > 0){
@@ -137,54 +121,74 @@ class NavigationController extends OntoWiki_Controller_Component
             }else{
                 return $uri;
             }
+        }else{
+            return "error";   
         }
     }
     
     protected function buildQuery($setup){
         $query = new Erfurt_Sparql_SimpleQuery();
-        $prologue = 'SELECT DISTINCT ?navEntry ';// ?sub ?subsub ?subsubsub ';
+        $prologue = 'SELECT DISTINCT ?navEntry ';
         $query->setProloguePart($prologue);
         
         $whereSpecs = array();
         
         // deeper qeury
         if ( isset($setup->state->parent) && ($setup->state->lastEvent == "navigateDeeper") ) {
-            $whereSpec = '?navEntry a <'.$setup->state->parent.'>. ';
-            $whereSpec .= ' FILTER (isURI(?navEntry))';
-            $query->setWherePart('WHERE {' . $whereSpec . '}');
-            $query->setLimit(10);;        
-            return $query;
-        }
-        
-        // Init query
-        foreach ($setup->config->hierarchyTypes as $type) {
-            $whereSpecs[] = '{?navEntry a <' . $type . '>}';
-        }
-        
-        if ( !isset($setup->state->parent) ) {
+            
             foreach($setup->config->hierarchyRelations->in as $rel){
                 if ($rel == "http://www.w3.org/2000/01/rdf-schema#subClassOf") {
-                    $whereSpecs[] = '{?instance a ?navEntry.}';
+                    $whereSpecs[] = '{?navEntry a <'.$setup->state->parent.'>.}';
                 }
                 // entities with a subtype must be a type
-                $whereSpecs[] = '{?subtype <' . $rel . '> ?navEntry.}';
+                $whereSpecs[] = '{?navEntry <' . $rel . '> <'.$setup->state->parent.'>.}';
+            }
+            
+        }else{
+                
+            // Init query
+            foreach ($setup->config->hierarchyTypes as $type) {
+                $whereSpecs[] = '{?navEntry a <' . $type . '>}';
+            }
+        
+            if ( !isset($setup->state->parent) ) {
+                foreach($setup->config->hierarchyRelations->in as $rel){
+                    if ($rel == "http://www.w3.org/2000/01/rdf-schema#subClassOf") {
+                        $whereSpecs[] = '{?instance a ?navEntry.}';
+                    }
+                    // entities with a subtype must be a type
+                    //$whereSpecs[] = '{?subtype <' . $rel . '> ?navEntry.}';
+                }
+            }
+        
+            // relations
+            if ( isset($setup->config->instanceRelation) ){
+                foreach($setup->config->instanceRelation->out as $rel){
+                    // entities must have a subtype
+                    $whereSpecs[] = '{?navEntry <' . $rel . '> ?subtype.}';
+                }        
             }
         }
         
         $whereSpec = implode(' UNION ', $whereSpecs);
-        $whereSpec .= ' FILTER (isURI(?navEntry))';
+        
+        // namespaces to be ignored, rdfs/owl-defined objects
+        if( isset($setup->config->hiddenRelation) ){
+            foreach ($setup->config->hiddenRelation as $ignore) {
+                $whereSpec .= ' OPTIONAL { ?navEntry <' . $ignore . '> ?reg . }';
+            }
+            $whereSpec .= 'FILTER (!bound(?reg))';
+        }
+
+        $whereSpec .= 'FILTER (isURI(?navEntry))';
         
         // dont't show rdfs/owl entities and subtypes in the first level
         if ( !isset($setup->state->parent) ) {
             $whereSpec .= ' FILTER (regex(str(?super), "^' . EF_OWL_NS . '") || !bound(?super))';
-        }
-        foreach($setup->config->hierarchyRelations->in as $rel){
-            /*$whereSpec .= ' OPTIONAL {?sub <' . $rel . '> ?navEntry.
-                                OPTIONAL {?subsub <' . $rel . '> ?sub.
-                                      OPTIONAL {?subsubsub <' . $rel . '> ?subsub}
-                                }
-                            }';*/
-            $whereSpec .= ' OPTIONAL {?navEntry <' . $rel . '> ?super. FILTER(isUri(?super))}';
+        
+            foreach($setup->config->hierarchyRelations->in as $rel){
+                $whereSpec .= ' OPTIONAL {?navEntry <' . $rel . '> ?super. FILTER(isUri(?super))}';
+            }
         }
         
         // namespaces to be ignored, rdfs/owl-defined objects
@@ -201,7 +205,7 @@ class NavigationController extends OntoWiki_Controller_Component
         
         $query->setWherePart('WHERE {' . $whereSpec . '}');
         
-        $query->setLimit(10);;        
+        $query->setLimit($this->limit);
         return $query;
     } 
 }
