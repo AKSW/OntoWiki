6a1d9d85f003381e8d9f3b3ee9bc333352a9a20c
Natanael Arndt <arndtn@gmail.com>
2009-11-10 22:47:23 +0100

    now using QueryObject in MapHelper

--- extensions/components/map/MapHelper.php
+++ extensions/components/map/MapHelper.php
@@ -20,6 +20,8 @@ require_once 'OntoWiki/Component/Helper.php';
 class MapHelper extends OntoWiki_Component_Helper
 {
 
+    private $instances = null;
+
     public function init()
     {
 
@@ -52,98 +54,118 @@ class MapHelper extends OntoWiki_Component_Helper
     {
 
         $owApp = OntoWiki::getInstance();
+        $session = $owApp->session;
 
-        //var_dump($owApp);
+        if (isset($session->instances)) {
 
-        if($owApp->selectedModel) {
+            $latProperties  = $this->_privateConfig->property->latitude->toArray();
+            $longProperties = $this->_privateConfig->property->longitude->toArray();
+            $latProperty    = $latProperties[0];
+            $longProperty   = $longProperties[0];
 
-            require_once 'Erfurt/Sparql/SimpleQuery.php';
+            $latVar         = new Erfurt_Sparql_Query2_Var('lat');
+            $longVar        = new Erfurt_Sparql_Query2_Var('long');
+            $lat2Var        = new Erfurt_Sparql_Query2_Var('lat2');
+            $long2Var       = new Erfurt_Sparql_Query2_Var('long2');
 
-            $store    = $owApp->erfurt->getStore();
-            $resource = (string) $owApp->selectedResource;
-
-            // build the query to get all marker resources
-            $query = new Erfurt_Sparql_SimpleQuery( );
-
-            $query->setProloguePart( 'SELECT ?p' );
-
-            $where = "WHERE {"      . EOL;
-            $where.= "	?s ?p ?o."  . EOL;
-            $where.= "	FILTER("    . EOL;
-
-            $latitude	= $this->_privateConfig->property->latitude->toArray();
-            $longitude	= $this->_privateConfig->property->longitude->toArray();
-
-            for ($i = 0; $i < count($latitude); $i++) {
-                $lat = $latitude[$i];
-                $long = $longitude[$i];
-                if( $i != 0 ) {
-                    $where.= " || ";
-                }
-                $where.= "		sameTerm(?p, <" . $lat . ">) ||" . EOL;
-                $where.= "		sameTerm(?p, <" . $long . ">)"   . EOL;
+            //the future is now!
+            if($this->instances === null) {    
+                $this->instances = clone $session->instances;
+                $owApp->logger->debug('MapHelper/shouldShow: clone this->_session->instances');
+            } else {
+                $owApp->logger->debug('MapHelper/shouldShow: this->instances already set');
+                // don't load instances again
             }
 
-            $where.= ") }";
+            $query          = $this->instances->getResourceQuery();
+            $owApp->logger->debug('MapHelper/shouldShow: session query: ' . var_export((string)$query, true));
 
-            $query->setWherePart($where);
-            $query->setLimit(1);
-            // ask for the properties
-            return $owApp->selectedModel->sparqlQuery($query);
+            $query->setQueryType(Erfurt_Sparql_Query2::typeSelect); /* would like to ask but ask lies */
+            $this->instances->setLimit(1);
+            $this->instances->setOffset(0);
+
+            $query->removeAllOptionals()->removeAllProjectionVars();
+
+            $query->addProjectionVar($this->instances->getResourceVar());
+            $query->addProjectionVar($latVar);
+            $query->addProjectionVar($longVar);
+            $query->addProjectionVar($lat2Var);
+            $query->addProjectionVar($long2Var);
+
+            $queryEu     = new Erfurt_Sparql_Query2_GroupGraphPattern();
+            $queryUsa     = new Erfurt_Sparql_Query2_GroupGraphPattern();
+
+            $node = new Erfurt_Sparql_Query2_Var('node'); // should be $node = new Erfurt_Sparql_Query2_BlankNode('bn'); but i heard this is not supported yet by zendb
+            $queryEu->addTriple($this->instances->getResourceVar(), $latProperty, $latVar);
+            $queryEu->addTriple($this->instances->getResourceVar(), $longProperty, $longVar);
+            $queryUsa->addTriple($this->instances->getResourceVar(), new Erfurt_Sparql_Query2_Var('pred') , $node);
+            $queryUsa->addTriple($node, $latProperty, $lat2Var);
+            $queryUsa->addTriple($node, $longProperty, $long2Var);
+
+            $queryUno     = new Erfurt_Sparql_Query2_GroupOrUnionGraphPattern();
+
+            $queryUno->addElement($queryEu)->addElement($queryUsa);
+            $query->addElement($queryUno);
+            $owApp->logger->debug('MapHelper/shouldShow: sent "' . $query . '" to know if SpacialThings are available.');
+
+            /* get result of the query */
+            $result    = $this->_owApp->erfurt->getStore()->sparqlQuery($query);
+
+            $owApp->logger->debug('MapHelper/shouldShow: got respons "' . var_export($result, true) . '".');
+
+            if ($result) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+
+            return $result;
         } else {
+            $owApp->logger->debug('MapHelper/shouldShow: no instances object set in session → no instances to show → no map.');
+
             return false;
+
+            /* old code */
+
+            if($owApp->selectedModel) {
+
+                require_once 'Erfurt/Sparql/SimpleQuery.php';
+
+                $store    = $owApp->erfurt->getStore();
+                $resource = (string) $owApp->selectedResource;
+
+                // build the query to get all marker resources
+                $query = new Erfurt_Sparql_SimpleQuery( );
+
+                $query->setProloguePart( 'SELECT ?p' );
+
+                $where = "WHERE {"      . EOL;
+                $where.= "	?s ?p ?o."  . EOL;
+                $where.= "	FILTER("    . EOL;
+
+                $latitude	= $this->_privateConfig->property->latitude->toArray();
+                $longitude	= $this->_privateConfig->property->longitude->toArray();
+
+                for ($i = 0; $i < count($latitude); $i++) {
+                    $lat = $latitude[$i];
+                    $long = $longitude[$i];
+                    if( $i != 0 ) {
+                        $where.= " || ";
+                    }
+                    $where.= "		sameTerm(?p, <" . $lat . ">) ||" . EOL;
+                    $where.= "		sameTerm(?p, <" . $long . ">)"   . EOL;
+                }
+
+                $where.= ") }";
+
+                $query->setWherePart($where);
+                $query->setLimit(1);
+                // ask for the properties
+                return $owApp->selectedModel->sparqlQuery($query);
+            } else {
+                return false;
+            }
         }
-
-        // this function should use the following code, but because there are no instances at the moment this wouldn't work
-
-        /*
-           $query = clone $this->_owApp->instances->getResourceQuery();
-           $query->removeAllOptionals()->removeAllProjectionVars();
-
-           $ggp1 = new Erfurt_Sparql_Query2_GroupGraphPattern();
-           $ggp2 = new Erfurt_Sparql_Query2_GroupGraphPattern(); 
-
-           $ggp1->addTriple(
-           $this->_owApp->instances->getResourceVar(),
-           'http://www.w3.org/2003/01/geo/wgs84_pos#long',
-           new Erfurt_Sparql_Query2_Var('long'));
-           $ggp1->addTriple(
-           $this->_owApp->instances->getResourceVar(),
-           'http://www.w3.org/2003/01/geo/wgs84_pos#lat',
-           new Erfurt_Sparql_Query2_Var('lat'));
-
-           $node = new Erfurt_Sparql_Query2_Var('node'); // should be $node = new Erfurt_Sparql_Query2_BlankNode('bn'); but i heard this is not supported yet by zendb
-           $ggp2->addTriple($this->_owApp->instances->getResourceVar(), new Erfurt_Sparql_Query2_Var('pred') , $node);
-           $ggp2->addTriple($node, 'http://www.w3.org/2003/01/geo/wgs84_pos#long', new Erfurt_Sparql_Query2_Var('long2'));
-           $ggp2->addTriple($node, 'http://www.w3.org/2003/01/geo/wgs84_pos#lat', new Erfurt_Sparql_Query2_Var('lat2'));
-
-           $union = new Erfurt_Sparql_Query2_GroupOrUnionGraphPattern(); 
-
-           $union->addElement($ggp1)->addElement($ggp2);
-
-           $query->addElement($union);
-           $query
-           ->setLimit(0)
-           ->setOffset(0)
-           ->removeAllProjectionVars()
-           ->removeAllOptionals();
-
-           $query->setQueryType(Erfurt_Sparql_Query2::typeAsk);
-        //echo htmlentities($query);
-
-        //for some reason sparqlAsk wants a SimpleQuery
-        $simpleQuery = Erfurt_Sparql_SimpleQuery::initWithString($query);
-        $ret = $this->_owApp->erfurt->getStore()->sparqlAsk($simpleQuery);
-        return is_bool($ret) ? $ret : false;
-
-        //or a lot easier if ignoring transitive map-infos
-        foreach($this->_owApp->instances->getAllProperties() as $property){
-        if($property['uri'] == 'http://www.w3.org/2003/01/geo/wgs84_pos#long' || $property['uri'] == 'http://www.w3.org/2003/01/geo/wgs84_pos#lat')
-        return true;
-        }
-
-        return false;
-         */
     }
 }
 
