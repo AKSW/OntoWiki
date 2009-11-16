e9f892c06b82d4abf14cc2400cb440a9dd5b59a5
Jonas Brekle <jonas.brekle@gmail.com>
2009-11-16 15:34:19 +0100

    update graphical query builder to zend and navigation changes fix query optimization in query2

---
+++ application/classes/OntoWiki/Controller/Plugin/ListSetupHelper.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * ListSetupHelper handles list
+ *
+ * @author jonas
+ */
+class OntoWiki_Controller_Plugin_ListSetupHelper extends Zend_Controller_Plugin_Abstract {
+    protected $_isSetup = false;
+    
+    /**
+     * RouteStartup is triggered before any routing happens.
+     */
+    public function routeStartup(Zend_Controller_Request_Abstract $request)
+    {
+        /**
+         * @trigger onRouteStartup
+         */
+        $event = new Erfurt_Event('onRouteStartup');
+        $event->trigger();
+    }
+
+    /**
+     * RouteShutdown is the earliest event in the dispatch cycle, where a
+     * fully routed request object is available
+     */
+    public function routeShutdown(Zend_Controller_Request_Abstract $request)
+    {
+        // only once
+        if (!$this->_isSetup) {
+            $frontController = Zend_Controller_Front::getInstance();
+            $ontoWiki        = OntoWiki::getInstance();
+            $store           = $ontoWiki->erfurt->getStore();
+
+            // instantiate model if parameter passed
+            if (isset($request->m)) {
+                try {
+                    $model = $store->getModel($request->getParam('m', null, false));
+                    $ontoWiki->selectedModel = $model;
+                } catch (Erfurt_Store_Exception $e) {
+                    // When no user is given (Anoymous) give the requesting party a chance to authenticate.
+                    if (Erfurt_App::getInstance()->getAuth()->getIdentity()->isAnonymousUser()) {
+                        // In this case we allow the requesting party to authorize...
+                        $response = $frontController->getResponse();
+
+                        $response->setRawHeader('HTTP/1.1 401 Unauthorized');
+                        $response->setHeader('WWW-Authenticate', 'FOAF+SSL');
+                        $response->sendResponse();
+                        exit;
+                    }
+
+                    // post error message
+                    $ontoWiki->prependMessage(new OntoWiki_Message(
+                        '<p>Could not instantiate model: ' . $e->getMessage() . '</p>' .
+                        '<a href="' . $ontoWiki->config->urlBase . '">Return to index page</a>',
+                        OntoWiki_Message::ERROR, array('escape' => false)));
+                    // hard redirect since finishing the dispatch cycle will lead to errors
+                    header('Location:' . $ontoWiki->config->urlBase . 'error/error');
+                    exit;
+                }
+            }
+
+            // instantiate resource if parameter passed
+            if (isset($request->r)) {
+                $graph = $ontoWiki->selectedModel;
+                if ($graph instanceof Erfurt_Rdf_Model) {
+                    $resource = new OntoWiki_Resource($request->getParam('r', null, true), $graph);
+                    $ontoWiki->selectedResource = $resource;
+                } else {
+                    // post error message
+                    $ontoWiki->prependMessage(new OntoWiki_Message(
+                        '<p>Could not instantiate resource. No model selected.</p>' .
+                        '<a href="' . $ontoWiki->config->urlBase . '">Return to index page</a>',
+                        OntoWiki_Message::ERROR, array('escape' => false)));
+                    // hard redirect since finishing the dispatch cycle will lead to errors
+                    header('Location:' . $ontoWiki->config->urlBase . 'error/error');
+                    exit;
+                }
+            }
+
+            /**
+             * @trigger onRouteShutdown
+             */
+            $event = new Erfurt_Event('onRouteShutdown');
+            $event->request = $request;
+            $event->trigger();
+
+            // avoid setting up twice
+            $this->_isSetup = true;
+        }
+    }
+}
+?>
--- application/tests/OntoWiki/InstancesTest.php
+++ application/tests/OntoWiki/InstancesTest.php
@@ -4,7 +4,7 @@ require_once '../test_base.php';
 
 // PHPUnit
 require_once 'PHPUnit/Framework.php';
-
+require_once '../../../erfurt/tests/Erfurt/Versioning/StoreStub.php';
 
 /**
  * InstancesTest tests the behavior of Ontowiki_Model_Instances
@@ -14,9 +14,11 @@ require_once 'PHPUnit/Framework.php';
 class InstancesTest extends PHPUnit_Framework_TestCase{
 
     protected $instances;
+    protected $_storeStub;
 
     public function setUp(){
-        $this->instances = new OntoWiki_Model_Instances(); //no config given
+        $this->_storeStub = new Erfurt_Versioning_StoreStub();
+        $this->instances = new OntoWiki_Model_Instances($this->_storeStub, "http://graph.com"); // no config given
     }
 
     public function testQuery(){
