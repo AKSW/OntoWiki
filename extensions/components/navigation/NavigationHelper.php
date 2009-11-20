22fdc9fb8de225f23ff77d05cc88ca0448195543
Sebastian Tramp <mail@sebastian.tramp.name>
2009-11-20 16:55:30 +0100

    cleanup

--- extensions/components/navigation/NavigationHelper.php
+++ extensions/components/navigation/NavigationHelper.php
@@ -1,26 +1,11 @@
 <?php
-
-
 /**
  * Nav helper. builds a query for the nav controller and for the resource controller
  * @category   OntoWiki
- * @package    OntoWiki_extensions_components_map
- * @author Norman Heino <norman.heino@gmail.com>
- * @version $Id: FoafeditHelper.php 3053 2009-05-08 12:15:51Z norman.heino $
+ * @package    OntoWiki_extensions_components_navigation
  */
 class NavigationHelper extends OntoWiki_Component_Helper
 {
-
-    public function init()
-    {
-        
-    }
-
-    public function shouldShow () 
-    {
-        return true; // right?
-    }
-
     public static function buildQuery($uri, $setup)
     {
         $searchVar = new Erfurt_Sparql_Query2_Var('resourceUri');
