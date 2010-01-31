2efed1a71f6cdb1a0cf3e658d071d42b9cf6f0e4
Natanael Arndt <arndtn@gmail.com>
2010-02-01 00:12:50 +0100

    updated jquery.json and moved navigation style from global css to module css

--- extensions/modules/tagging/tagging.php
+++ extensions/modules/tagging/tagging.php
@@ -17,6 +17,7 @@ class TaggingModule extends OntoWiki_Module
 {
     public function init() {
         $this->view->headScript()->appendFile($this->view->moduleUrl . 'tagging.js');
+        $this->view->headLink()->appendStylesheet($this->view->moduleUrl . 'tagging.css', 'screen');
     }
     
 	
