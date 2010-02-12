1eda2bdf6fc57295ea3eacf3d907970ce3c018ba
Philipp Frischmuth <pfrischmuth@googlemail.com>
2010-02-12 16:44:24 +0100

    pingback plugin log

--- extensions/plugins/pingback/pingback.php
+++ extensions/plugins/pingback/pingback.php
@@ -217,7 +217,7 @@ class PingbackPlugin extends OntoWiki_Plugin
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $res = curl_exec($rq);
         curl_close($rq);
-		$this->_logInfo('Pingback Result - ' . $res);
+		$this->_logInfo('Pingback Result for ('.$pingbackServiceUrl.') - ' . $res);
 		
 		return true;
 	}
