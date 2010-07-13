02fbf3d2f1e6c3aa5589fe2985184917477bf5e0
Norman Heino <norman.heino@gmail.com>
2010-07-13 17:24:22 +0200

    Added: initial component structure for CSV Import wizard

---
+++ extensions/components/csvimport/CsvimportController.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ *
+ * @copyright Copyright (c) 2008, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+
+/**
+ * Component controller for the CSV Importer.
+ *
+ * @category OntoWiki
+ * @package Extensions
+ * @subpackage Csvimport
+ * @copyright Copyright (c) 2010, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+class CsvimportController extends OntoWiki_Controller_Component
+{
+    public function init()
+    {
+        
+    }
+    
+    public function importAction()
+    {
+        // // TODO: show import dialogue and import file
+    }
+    
+    public function mappingAction()
+    {
+        // // TODO: show table and let user define domain mapping
+    }
+}
---
+++ extensions/components/csvimport/CsvimportHelper.php
@@ -0,0 +1,25 @@
+<?php 
+
+/**
+ * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ *
+ * @copyright Copyright (c) 2008, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+
+ /**
+  * CSV Importer component helper.
+  *
+  * @category OntoWiki
+  * @package Extensions
+  * @subpackage Csvimport
+  * @copyright Copyright (c) 2010, {@link http://aksw.org AKSW}
+  * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+  */
+class CsvimportHelper extends OntoWiki_Component_Helper
+{
+    public function init()
+    {
+        // TODO: register menu entry
+    }
+}
