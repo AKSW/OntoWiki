d94a46657c22226e31908c01e25d74fa93f9cacf
Norman Heino <norman.heino@gmail.com>
2010-07-14 17:34:38 +0200

    Added: mockup data

--- extensions/components/csvimport/CsvimportController.php
+++ extensions/components/csvimport/CsvimportController.php
@@ -18,18 +18,97 @@
  */
 class CsvimportController extends OntoWiki_Controller_Component
 {
+    protected $_dimensions = null;
+    protected $_columnMappings = null;
+    protected $_targetGraph = null;
+    
     public function init()
     {
-        
+        // init component
     }
     
     public function importAction()
     {
-        // // TODO: show import dialogue and import file
+        // TODO: show import dialogue and import file
     }
     
     public function mappingAction()
     {
-        // // TODO: show table and let user define domain mapping
+        // TODO: show table and let user define domain mapping
+    }
+    
+    protected function _getColumnMapping()
+    {
+        $columnMapping = array(
+            array(
+                'property' => 'http://xmlns.com/foaf/0.1/name', 
+                'label' => 'Name', 
+                'col' => 3, 
+                'row' => 2, 
+                'items' => array(
+                    'type' => 'uri', 
+                    'class' => 'http://xmlns.com/foaf/0.1/Person', 
+                    'start' => array('col' => 3, 'row' => 2), 
+                    'end' => array('col' => 3, 'row' => 20)
+                ), 
+            ), 
+            array(
+                'property' => 'http://purl.org/dc/elements/1.1/', 
+                'label' => 'Titel', 
+                'col' => 4, 
+                'row' => 2,
+                'items' => array(
+                    'type' => 'literal', 
+                    'datatype' => 'http://www.w3.org/2001/XMLSchema#string', 
+                    'start' => array('col' => 4, 'row' => 2), 
+                    'end' => array('col' => 4, 'row' => 20)
+                )
+            )
+        );
+    }
+    
+    protected function _getDimensions()
+    {
+        $dimensions = array(
+            'http://example.com/dimension1' => array(
+                'label' => 'Age', 
+                'elements' => array(
+                    'http://example.com/dimension1/0-6' => array(
+                        'col' => 2, 
+                        'row' => 2, 
+                        'label' => '0-6', 
+                        'items' => array(
+                            'start' => array('col' => 2, 'row' => 3), 
+                            'end'   => array('col' => 2, 'row' => 20)
+                        )
+                    )
+                ), 
+                'http://example.com/dimension1/7-12' => array(
+                    'col' => 3, 
+                    'row' => 2, 
+                    'label' => '7-12', 
+                    'items' => array(
+                        'start' => array('col' => 3, 'row' => 3), 
+                        'end'   => array('col' => 3, 'row' => 20)
+                    )
+                )
+            ),  
+            'http://example.com/dimension2' => array(
+                'label' => 'Region', 
+                'elements' => array(
+                    'http://example.com/dimension2/Africa' => array(
+                        'col' => 1, 
+                        'row' => 3, 
+                        'label' => 'Africa', 
+                        'items' => array(
+                            'start' => array('col' => 2, 'row' => 3), 
+                            'end'   => array('col' => 2, 'row' => 20)
+                        )
+                    )
+                )
+            )
+        );
+        
+        return $dimensions;
     }
 }
