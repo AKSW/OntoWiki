bdf8db38f68e022539c73f97d93b316f9cdad947
Sebastian Tramp <mail@sebastian.tramp.name>
2010-07-21 22:24:38 +0200

    site: model config value now autoselected

--- extensions/components/site/SiteController.php
+++ extensions/components/site/SiteController.php
@@ -60,26 +60,6 @@ class SiteController extends OntoWiki_Controller_Component
         parent::init();
         $this->_helper->viewRenderer->setNoRender();
         $this->_helper->layout()->disableLayout();
-
-        // m is automatically used and selected
-        if ((!isset($this->_request->m)) && (!$this->_owApp->selectedModel)) {
-            throw new OntoWiki_Exception('No model pre-selected model and missing parameter m (model)!');
-            exit;
-        } else {
-            $this->_model = $this->_owApp->selectedModel;
-            $this->_modelUri = (string) $this->_owApp->selectedModel;
-        }
-
-        // r is automatically used and selected
-        if ((!isset($this->_request->r)) && (!$this->_owApp->selectedResource)) {
-            throw new OntoWiki_Exception('No resource pre-selected resource and missing parameter r (resource)!');
-            exit;
-        } else {
-            $this->_resource = $this->_owApp->selectedResource;
-
-            $this->_resourceUri = (string) $this->_owApp->selectedResource;
-        }
-
     }
 
     /*
@@ -92,26 +72,52 @@ class SiteController extends OntoWiki_Controller_Component
         $mainTemplate = sprintf('%s/%s', $this->_site, self::MAIN_TEMPLATE_NAME);
         
         if (is_readable($templatePath . $mainTemplate)) {
+            
             $moduleContext = 'site.' . $this->_site;
             // $this->addModuleContext($moduleContext);
-            
+
             $siteConfig = array(
                 'id'          => $this->_site,
                 'basePath'    => sprintf('%s/sites/%s', $this->_componentRoot, $this->_site),
                 'baseUri'     => sprintf('%s/sites/%s/', $this->_componentUrlBase, $this->_site),
-                'resourceUri' => $this->_resourceUri,
                 'context'     => $moduleContext,
                 'privateConfig' => array()
             );
+
+            // load the site config
             $configFileName = $this->_componentRoot.'/sites/'.$this->_site.'/config.ini';
             if(is_readable($configFileName)){
                 $ini =  parse_ini_file($configFileName, true);
                 if(is_array($ini)){
                     $siteConfig['privateConfig'] = $ini;
-                } 
+                }
             }
 
-            $navigation = $this->getSiteModelAsArray();
+            // m is automatically used and selected
+            if ((!isset($this->_request->m)) && (!$this->_owApp->selectedModel)) {
+                if (!Zend_Uri::check($siteConfig['privateConfig']['model'])) {
+                    throw new OntoWiki_Exception('No model pre-selected model, no parameter m (model) and no configured site model!');
+                } else {
+                    // setup the model
+                    $this->_modelUri = $siteConfig['privateConfig']['model'];
+                    $store = OntoWiki::getInstance()->erfurt->getStore();
+                    $this->_model = $store->getModel($this->_modelUri);
+                    OntoWiki::getInstance()->selectedModel = $this->_model;
+                }
+            } else {
+                $this->_model = $this->_owApp->selectedModel;
+                $this->_modelUri = (string) $this->_owApp->selectedModel;
+            }
+
+            // r is automatically used and selected, if not then we use the model uri as starting point
+            if ((!isset($this->_request->r)) && (!$this->_owApp->selectedResource)) {
+                OntoWiki::getInstance()->selectedResource = $this->_model->getResource($this->_modelUri);
+            }
+            $this->_resource = $this->_owApp->selectedResource;
+            $this->_resourceUri = (string) $this->_owApp->selectedResource;
+            $siteConfig['resourceUri']  = $this->_resourceUri;
+            
+            $navigation = $this->getSiteNavigationAsArray();
             $siteConfig['navi'] = $navigation;
 
             // mit assign kann man im Template direkt zugreifen ($this->basePath).
@@ -122,8 +128,10 @@ class SiteController extends OntoWiki_Controller_Component
             $this->_response->setBody($this->view->render('404.phtml'));
         }
     }
-    protected function getSiteModelAsArray(){
-        $store    = OntoWiki::getInstance()->erfurt->getStore();
+
+    
+    protected function getSiteNavigationAsArray(){
+        $store = OntoWiki::getInstance()->erfurt->getStore();
         $model = $this->_owApp->selectedModel;
         $query = 'PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
             SELECT ?topconcept FROM <'.(string)$model.'> WHERE {
