f7e0f29993566bea1cd5a2b095114bdc9c71be1a
Norman Heino <norman.heino@gmail.com>
2010-07-22 14:59:18 +0200

    Code cleanup

--- extensions/plugins/linkeddata/linkeddata.php
+++ extensions/plugins/linkeddata/linkeddata.php
@@ -98,7 +98,7 @@ class LinkeddataPlugin extends OntoWiki_Plugin
                     // make active graph (session required)
                     $activeModel = $store->getModel($graph);
                     OntoWiki::getInstance()->selectedModel    = $activeModel;
-                    OntoWiki::getInstance()->selectedResource = $activeModel->getResource($uri);
+                    OntoWiki::getInstance()->selectedResource = new OntoWiki_Resource($uri, $activeModel);
                     // default property view
                     $url = new OntoWiki_Url(array('route' => 'properties'), array());
                     $url->setParam('r', $uri, true)
