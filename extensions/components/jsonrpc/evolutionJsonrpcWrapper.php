03911d10dc3e998ddc7dcc9e7759c08bd562f9c8
Marvin Frommhold <marvin.frommhold@gmail.com>
2010-08-12 18:37:23 +0200

    evolutionWrapper

---
+++ extensions/components/jsonrpc/evolutionJsonrpcWrapper.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * JSON RPC Class, this wrapper class is for all Evolution Engine RPC calls.
+ *
+ * @category	OntoWiki
+ * @package		extensions_components_jsonrpc
+ * @copyright	Copyright (c) 2010, {@link http://aksw.org AKSW}
+ * @license		http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @author		Marvin Frommhold
+ */
+
+class evolutionJsonrpcWrapper
+{
+
+	public function __construct() {
+
+	}
+
+	/**
+	 * @desc executes the given pattern
+	 * @return string
+	 */
+	//public function execPattern($pattern, $graph, $variables = null) {
+	public function execPattern() {
+
+		return "Hallo RPC";
+	}
+}
+?>
+
