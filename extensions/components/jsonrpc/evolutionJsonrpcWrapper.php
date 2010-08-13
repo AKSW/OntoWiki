29d737392b87d6c4974c16271cf514192e2d82a2
Marvin Frommhold <marvin.frommhold@gmail.com>
2010-08-13 20:48:41 +0200

    mod: add function execPattern to evolution wrapper

--- extensions/components/jsonrpc/evolutionJsonrpcWrapper.php
+++ extensions/components/jsonrpc/evolutionJsonrpcWrapper.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * JSON RPC Class, this wrapper class is for all Evolution Engine RPC calls.
  *
@@ -9,22 +10,82 @@
  * @author		Marvin Frommhold
  */
 
+require_once 'extensions/components/patternmanager/classes/PatternEngine.php';
+require_once 'extensions/components/patternmanager/classes/ComplexPattern.php';
+require_once 'extensions/components/patternmanager/classes/BasicPattern.php';
+require_once 'extensions/components/patternmanager/classes/PatternFunction.php';
+
 class evolutionJsonrpcWrapper
 {
 
+	private $owApp = null;
+	private $engine = null;
+
 	public function __construct() {
 
+		$this->owApp = OntoWiki::getInstance();
+
+		$patternManagerConfig = $this->owApp->componentManager->getComponentPrivateConfig("patternmanager");
+
+		$this->engine = new PatternEngine();
+		$this->engine->setConfig($patternManagerConfig);
+		$this->engine->setBackend($this->owApp->erfurt);
 	}
 
 	/**
-	 * @desc executes the given pattern
+	 * @desc Executes the given Evolution pattern (in JSON format).
+	 * @param params		the POST data parameters (structure: [{"pattern":"foo","graph":"foo","variables":{"var1":"foo","var2":"foo"}}], variables is not mandatory)
 	 * @return string
+	 * @throws Exception
 	 */
-	//public function execPattern($pattern, $graph, $variables = null) {
-	public function execPattern() {
+	public function execPattern($params) {
 
-		return "Hallo RPC";
+		// get pattern
+		if ( !array_key_exists("pattern", $params)) {
+
+			throw new Erfurt_Exception("No pattern given!");
+		}
+		$pattern = urldecode($params['pattern']);
+
+		// get graph
+		if ( !array_key_exists("graph", $params) ) {
+
+			throw new Erfurt_Exception("No graph given!");
+		}
+		$graph = $params["graph"];
+
+		// get variables
+		$variables = array();
+		if ( array_key_exists("variables", $params) ) {
+
+			$variables = $params["variables"];
+		}
+
+		// set graph
+		$this->engine->setDefaultGraph($graph);
+
+		// create pattern
+		$complexPattern = new ComplexPattern();
+		// load pattern from json string
+		$complexPattern->fromArray($pattern, true);
+
+		// bound variables
+ 		$unboundVariables = $complexPattern->getVariables(false);
+
+ 		if (!empty($variables) && is_array($variables)) {
+
+			foreach ($variables as $name => $value) {
+				unset($unboundVariables[$name]);
+				$complexPattern->bindVariable($name,$value);
+			}
+
+			// process the pattern
+			$this->engine->processPattern($complexPattern);
+        }
+
+		return "Success";
 	}
 }
+
 ?>
 
