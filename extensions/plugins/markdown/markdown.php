b0be100db69005baaee16b957cc3cd2a8a23bfff
Michael Martin <martin@informatik.uni-leipzig.de>
2010-09-08 14:59:23 +0200

    small changes in markdown widget

--- extensions/plugins/markdown/markdown.php
+++ extensions/plugins/markdown/markdown.php
@@ -1,24 +1,18 @@
 <?php
-
 /**
  * @category   OntoWiki
  * @package    OntoWiki_extensions_plugins
  */
-class MarkdownPlugin extends OntoWiki_Plugin
-{
+class MarkdownPlugin extends OntoWiki_Plugin {
 
-	protected $_config = null;
-
-    public function init()
-    {
+    public function init() {
         $this->properties = array_values($this->_privateConfig->properties->toArray());
     }
 
-    public function onDisplayLiteralPropertyValue($event)
-    {
-		if (in_array($event->property, $this->properties)) {
-			require_once("parser/markdown.php");
-			return Markdown($event->value);
-		}
+    public function onDisplayLiteralPropertyValue($event) {
+	if (in_array($event->property, $this->properties)) {
+            require_once("parser/markdown.php");
+            return Markdown($event->value);
+	}
     }
 }
