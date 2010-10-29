ca9bfbc24935aa7ec466f3ac3673798ec9c39f03
Sebastian Tramp <mail@sebastian.tramp.name>
2010-10-29 14:29:47 +0200

    add: querylist accepts results instead of query objects now (with isResult = true key)

--- extensions/components/site/sites/spiders/js/spiders.js
+++ extensions/components/site/sites/spiders/js/spiders.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-	$('.checklist').dataTable( {
-        "sDom": '<"top"if>rt<"bottom"lp<"clear">',
-		"bPaginate": false,
-		"bLengthChange": true,
-		"bFilter": true,
-		"bSort": true,
-		"bInfo": true,
-		"bAutoWidth": true } );
+    $('.checklist').dataTable( {
+    "sDom": '<"top"if>rt<"bottom"lp<"clear">',
+            "bPaginate": false,
+            "bLengthChange": true,
+            "bFilter": true,
+            "bSort": true,
+            "bInfo": true,
+            "bAutoWidth": true } );
+
+    $('span.CitedRecordSum').text($("td.CitedRecordCount").sum());
+    $('span.OriginalRecordSum').text($("td.OriginalRecordCount").sum());
 
 } );
