d5d7d5607175ce1bb8a8c61f8c2c6ff2777b4316
Christoph Rieß <c.riess.dev@googlemail.com>
2010-11-08 16:21:41 +0800

    [fix] ... smaller issues

--- extensions/components/patternmanager/scripts/patternmanager-browse.js
+++ extensions/components/patternmanager/scripts/patternmanager-browse.js
@@ -1,21 +1,28 @@
 // Configure exploretags module via jscript only
 $(document).ready(function() {
     if ( typeof reloadExploreTagsModule === 'function' && $('#exploretags') !== null ) {
-        var data = {
-          uri :  evopatLevelUri ,
-          label : 'Pattern Level' ,
-          isInverse : false
-        };
-        var getUri = urlBase+"service/session?method=setArrayValue&name=cloudproperties&valueIsSerialized=true";
-        $.post(getUri,
-         {
-             "key": evopatLevelUri,
-             "value" : $.toJSON(data)
-         }, //as post because of possible size
-        function(res) {
-            if(res==""){
-                reloadExploreTagsModule();
-            } else alert('could not add cloudproperty\nReason:\n'+res)
-        });
+        
+        if ( $('#exploretags *[title="Pattern Level"]').empty() ) {
+            
+            var data = {
+                uri :  evopatLevelUri ,
+                label : 'Pattern Level' ,
+                isInverse : false
+            };
+            var getUri = urlBase+"service/session?method=setArrayValue&name=cloudproperties&valueIsSerialized=true";
+            $.post(getUri,
+                {
+                   "key": evopatLevelUri,
+                   "value" : $.toJSON(data)
+               }, //as post because of possible size
+              function(res) {
+                  if(res==""){
+                      reloadExploreTagsModule();
+                  } else alert('could not add cloudproperty\nReason:\n'+res)
+              });
+            
+        } else {
+            // do nothing 
+        }
     }
 });
