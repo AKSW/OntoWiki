bcebba02d53ac43c0ad19a212117f4f0f63fa1e8
Norman Heino <norman.heino@gmail.com>
2010-11-22 17:57:12 +0100

    Added source test

---
+++ extensions/components/source/tests/SourceControllerTest.php
@@ -0,0 +1,27 @@
+<?php 
+
+require_once '../../../../application/tests/test_base.php';
+
+class SourceControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+    public function setUp()
+    {
+        $this->bootstrap = new Zend_Application(
+            'default',
+            ONTOWIKI_ROOT . 'application/config/application.ini'
+        );
+        parent::setUp();
+    }
+    
+    public function testDispatching()
+    {
+        $this->dispatch('/source/edit');
+        $this->request->setParam('m', 'http://localhost/OntoWiki/Config/');
+        
+        $r = $this->getResponse();
+        var_dump($r->getBody());
+        
+        $this->assertController('source');
+        $this->assertAction('edit');
+    }
+}
