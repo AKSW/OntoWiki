89c2cb0503c8b063624c154e68c5a29fece8d275
Sebastian Tramp <mail@sebastian.tramp.name>
2011-02-03 03:43:18 +0100

    init: species map

--- extensions/components/site/sites/spiders/js/spiders.js
+++ extensions/components/site/sites/spiders/js/spiders.js
@@ -6,9 +6,33 @@ $(document).ready(function() {
             "bFilter": true,
             "bSort": true,
             "bInfo": true,
-            "bAutoWidth": true } );
+            "bAutoWidth": true} );
 
     $('span.CitedRecordSum').text($("td.CitedRecordCount").sum());
     $('span.OriginalRecordSum').text($("td.OriginalRecordCount").sum());
 
-} );
+    var mapLocations = new Array();
+    $("a.location").each(function (i) {
+        var lat = $(this).find('span.lat').text();
+        var lon = $(this).find('span.long').text();
+        var uri = $(this).attr('about');
+
+        if ((lat.length > 0) && (lon.length > 0)) {
+            marker = {
+                latitude: lat,
+                longitude: lon
+            }
+            mapLocations.push(marker);
+        }
+    });
+
+    $('div.speciesmap').height(300);
+    $('div.speciesmap').gMap({
+        markers: mapLocations,
+        maptype: G_PHYSICAL_MAP,
+        latitude: 41.989722,
+        longitude: 43.59,
+        zoom: 6
+    });
+});
+
