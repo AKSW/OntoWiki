4ed7c5d7daba7a482c3065cb1cab79169ae057a7
Sebastian Tramp <mail@sebastian.tramp.name>
2011-02-05 02:08:46 +0100

    add: marker clickable and equipped with filter and location link

--- extensions/components/site/sites/spiders/js/spiders.js
+++ extensions/components/site/sites/spiders/js/spiders.js
@@ -16,13 +16,36 @@ $(document).ready(function() {
         var lat = $(this).find('span.lat').text();
         var lon = $(this).find('span.long').text();
         var uri = $(this).attr('about');
+        var name = $(this).find('span.name').text();
+        var html = '<p><strong about"'+ uri +'">' + name + '</strong></p>'
+            +'<ul>'
+            +'<li><a href="'+uri+'">go to place</a></li>'
+            +'<li><a href="javascript:filterList(\''+name+'\')">filter list</a></li>'
+            +'</ul>';
+
+        var iconL1 = {
+                    shadow:             false,
+                    iconanchor:         [4, 19],
+                    infowindowanchor:   [8, 2],
+                    image:              "https://chart.googleapis.com/chart?chst=d_map_spin&chld=0.2|330|FF0000|1"
+                };
+        var iconL2 = {
+                    shadow:             false,
+                    iconanchor:         [4, 19],
+                    infowindowanchor:   [8, 2],
+                    image:              "https://chart.googleapis.com/chart?chst=d_map_spin&chld=0.2|330|FF00FF|1"
+                };
+        
 
         if ((lat.length > 0) && (lon.length > 0)) {
-            marker = {
+            m = {
+                count: 1,
+                html: html,
                 latitude: lat,
-                longitude: lon
-            }
-            mapLocations.push(marker);
+                longitude: lon,
+                icon: iconL1
+            };
+            mapLocations.push(m);
         }
     });
 
@@ -30,9 +53,14 @@ $(document).ready(function() {
     $('div.speciesmap').gMap({
         markers: mapLocations,
         maptype: G_PHYSICAL_MAP,
+        scrollwheel: false,
         latitude: 41.989722,
         longitude: 43.59,
         zoom: 6
     });
+    
 });
 
+function filterList (name) {
+    $('div.dataTables_filter').find('input').attr('value', name).trigger('keyup');
+}
