6dca54ace599a82f9b99280c7e8a55add7c30675
Sebastian Tramp <mail@sebastian.tramp.name>
2011-04-21 15:46:14 +0200

    init activity filter module mockup

---
+++ extensions/dssn/ActivityfilterModule.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * DSSN module – Activity Filter
+ *
+ * @category   OntoWiki
+ * @package    OntoWiki_extensions_modules_activityfilter
+ * @copyright  Copyright (c) 2011, {@link http://aksw.org AKSW}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+class ActivityfilterModule extends OntoWiki_Module
+{
+    public function getTitle()
+    {
+        return 'Filter Activities';
+    }
+
+    public function init()
+    {
+        /* nothing to do right now */
+    }
+
+    public function shouldShow() {
+        // module can be turned off in extension config
+        if ($this->_privateConfig->modules->activityfilter != true) {
+            return false;
+        }
+        return true;
+    }
+
+    function getContents()
+    {
+        $content = $this->render('modules/activityfilter', false, 'activityfilter');
+        return $content;
+    }
+}
+
+
