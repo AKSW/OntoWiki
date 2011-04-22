9f7047a16712ee0733b8df171cf7d05ff0cfb642
Sebastian Tramp <mail@sebastian.tramp.name>
2011-04-22 20:43:11 +0200

    mockup: make friends

---
+++ extensions/dssn/MakefriendsModule.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * DSSN module – Make Friends
+ *
+ * @category   OntoWiki
+ * @package    OntoWiki_extensions_modules_bdays
+ * @copyright  Copyright (c) 2011, {@link http://aksw.org AKSW}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+class MakefriendsModule extends OntoWiki_Module
+{
+    public function getTitle()
+    {
+        return 'Make new Friends :-)';
+    }
+
+    public function init()
+    {
+        /* nothing to do right now */
+    }
+
+    public function shouldShow() {
+        // module can be turned off in extension config
+        if ($this->_privateConfig->modules->makefriends != true) {
+            return false;
+        }
+        return true;
+    }
+
+    function getContents()
+    {
+        $content = $this->render('modules/makefriends', false, 'makefriends');
+        return $content;
+    }
+}
+
+
