c5af0a9de9ef0e21f446e9c710a667f51129be0e
Lars Eidam <larseidam@googlemail.com>
2011-07-18 15:19:34 +0200

    - add: PHP CodeSniffer support - chg: Makefile to easy use CodeSniffer - add: scripts to easy add and delete pre commit hook - add: Ontowiki Coding Standard in a early version

--- Makefile
+++ Makefile
@@ -14,6 +14,16 @@ default:
 	@echo "     'make directories' (create cache/log dir and chmod environment)"
 	@echo "     'make install' (-> make directories, zend and libraries)"
 	@echo "     'make clean' (deletes all log and cache files)"
+	@echo "     'make cs-install' (install CodeSniffer)"
+	@echo "     'make cs-uninstall' (uninstall CodeSniffer)"
+	@echo "     'make cs-enable' (enable CodeSniffer to check code before every commit)"
+	@echo "     'make cs-disable' (disable CodeSniffer code checking)"
+	@echo "     'make cs-check-commit' (run pre ciommit code checking manually)"
+	@echo "     'make cs-check-commit-intensive' (run pre ciommit code checking"
+	@echo "             manually with stricter coding standard)"
+	@echo "     'make cs-check-all' (run complete code checking)"
+	@echo "     'make cs-check-commit-intensive' (run complete code checking with"
+	@echo "             stricter coding standard)"
 
 
 # top level target
@@ -93,3 +103,38 @@ debianize:
 	rm Makefile
 	@echo "now do: cp -R application/scripts/debian debian"
 
+
+# coding standard
+
+# #### config ####
+# if severity classes were chanced aou must run 'cs-install' again
+# standard severity class they must be fulfilled to be able to commit
+severity = 7
+# intensive severity class they must not be fulfilled to be able to commit,
+# but you are able to check your code with additional coding standards
+severity_intensive = 5
+# checkt filetypes
+filetypes = php
+# path to the Ontowiki Coding Standard
+cspath = application/tests/CodeSniffer/Standards/Ontowiki
+
+cs-install: cs-enable
+	pear install PHP_CodeSniffer
+
+cs-uninstall: cs-disable
+
+cs-enable:
+	./application/tests/CodeSniffer/add-hook.sh
+
+cs-disable:
+	./application/tests/CodeSniffer/remove-hook.sh
+
+cs-check-commit:
+	hg status -nam | grep '\.$(filetypes)$\' | xargs --no-run-if-empty phpcs --report=full --severity=$(severity) -p -s --standard=$(cspath)
+cs-check-commit-intensive:
+	hg status -nam | grep '\.$(filetypes)$\' | xargs --no-run-if-empty phpcs --report=full --severity=$(severity_intensive) -p -s --standard=$(cspath)
+
+cs-check-all:
+	phpcs --report=summary --extensions=$(filetypes) --severity=$(severity) -s -p --standard=$(cspath) *
+cs-check-all-intensive:
+	phpcs --report=summary --extensions=$(filetypes) --severity=$(severity_intensive) -s -p --standard=$(cspath) *
---
+++ application/tests/CodeSniffer/Standards/Ontowiki/Sniffs/Classes/ClassFilePathSniff.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Ontowiki_Sniffs_Classes_ClassFilePathSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Lars Eidam <lars.eidam@googlemail.com>
+ * @copyright 
+ * @license   
+ * @version   
+ * @link      http://code.google.com/p/ontowiki/
+ */
+
+/**
+ * Ontowiki_Sniffs_Classes_ClassFilePathSniff.
+ *
+ * Tests that the filepath correspond to the classname for php Files in
+ * the application/classes Folder
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Lars Eidam <lars.eidam@googlemail.com>
+ * @copyright 
+ * @license   
+ * @version   
+ * @link      http://code.google.com/p/ontowiki/
+ */
+class Ontowiki_Sniffs_Classes_ClassFilePathSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_CLASS,
+               );
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens   = $phpcsFile->getTokens();
+        $decName  = $phpcsFile->findNext(T_STRING, $stackPtr);
+        $fullPath = $phpcsFile->getFilename();
+		$longFileName = basename($fullPath);
+        $fileName = substr($longFileName, 0, strrpos($longFileName, '.'));
+
+		// if the file is under the application/classes folder the class has the path in the name
+		// application/classes/Ontowiki/Utils/TestClass.php -> Classname=Ontowiki_Utils_TestClass
+		if (stristr($fullPath, 'classes') !== FALSE) {
+			$partedPath = substr($fullPath, strrpos($fullPath, 'classes'), strlen($fullPath));
+				$partedPath = substr($partedPath, 0, strrpos($partedPath, '.'));
+
+			$classNameArray = explode("_", $tokens[$decName]['content']);
+			$filepathArray = explode("/", $partedPath);
+			if (1 == count($filepathArray)) {
+				$filepathArray = explode("\\", $partedPath);
+			}
+			
+			$notFound = TRUE;
+			foreach ($classNameArray as $index => $classNamePart) {
+				if ($classNamePart != $filepathArray[$index + 1]) {
+					$notFound = FALSE;
+					break;
+				}
+			}
+			if (FALSE === $notFound) {
+				$error = '%s name doesn\'t match filepath; expected "%s %s"';
+				$data  = array(
+						  ucfirst($tokens[$stackPtr]['content']),
+						  $tokens[$stackPtr]['content'],
+						  $tokens[$decName]['content'],
+						 );
+				$phpcsFile->addError($error, $stackPtr, 'NoMatch', $data);
+			}
+		}
+	//	else
+	//	{
+	//		if ($tokens[$decName]['content'] !== $fileName)
+	//		{
+	//			$error = '%s name doesn\'t match filename; expected "%s %s"';
+	//			$data  = array(
+	//				ucfirst($tokens[$stackPtr]['content']),
+	//				$tokens[$stackPtr]['content'],
+	//				$fileName,
+	//				);
+	//			$phpcsFile->addError($error, $stackPtr, 'NoMatch', $data);
+	//		}
+	//	}
+    }//end process()
+
+
+}//end class
+
+?>
