a8df591ccf7197e70793114fb7d3a3eba3918191
Sebastian Tramp <mail@sebastian.tramp.name>
2011-09-08 11:53:14 +0200

    add improved literal markup helper

--- extensions/site/helpers/Literal.php
+++ extensions/site/helpers/Literal.php
@@ -7,15 +7,17 @@
  */
 
 /**
- * OntoWiki LiteralTag view helper
+ * OntoWiki Literal view helper
  *
  * returns the content of a specific property of a given resource as an RDFa 
- * annotated tag with (optional) given css classes
+ * annotated tag with (optional) given css classes and other parameters
+ * this helper is usable as {{literal ...}} markup in combination with
+ * ExecuteHelperMarkup
  *
  * @category OntoWiki
  * @package  OntoWiki_extensions_components_site
  */
-class Site_View_Helper_LiteralTag extends Zend_View_Helper_Abstract
+class Site_View_Helper_Literal extends Zend_View_Helper_Abstract
 {
     /*
      * current view, injected with setView from Zend
@@ -32,45 +34,57 @@ class Site_View_Helper_LiteralTag extends Zend_View_Helper_Abstract
 
     /*
      * the main tah method, mentioned parameters are:
+     * - uri - which resource the literal is from (empty means selected * Resource)
+     * - property - qname/uri of property to use
      * - class (css class)
-     * - tag (the used tag, e.g. div)
-     * - prefix
-     * - suffix
-     * - iprefix
-     * - isuffix
+     * - tag (the used tag, e.g. span)
+     * - prefix - string at the beginning
+     * - suffix - string at the end
+     * - iprefix - string between tag and content at the beginning
+     * - isuffix - string betwee content and tag at the end
      */
-    public function literalTag($desc = null, $contentProperties = null, $options = array())
+    public function literal($options = array())
     {
         $store       = OntoWiki::getInstance()->erfurt->getStore();
         $model       = OntoWiki::getInstance()->selectedModel;
         $titleHelper = new OntoWiki_Model_TitleHelper($model);
 
-        if (!$desc) {
-            return '';
-        }
-
         // check for options and assign local vars or default values
         $class   = (isset($options['class']))   ? $options['class']   : '';
-        $tag     = (isset($options['tag']))     ? $options['tag']     : 'div';
+        $tag     = (isset($options['tag']))     ? $options['tag']     : 'span';
         $prefix  = (isset($options['prefix']))  ? $options['prefix']  : '';
         $suffix  = (isset($options['suffix']))  ? $options['suffix']  : '';
         $iprefix = (isset($options['iprefix'])) ? $options['iprefix'] : '';
         $isuffix = (isset($options['isuffix'])) ? $options['isuffix'] : '';
 
-        if (!$contentProperties) {
-            // used default property resources
-            $contentProperties = $this->contentProperties;
-        } else if (is_string($contentProperties)) {
-            // string to array
-            $tmpArray = array();
-            $tmpArray[] = Erfurt_Uri::getFromQnameOrUri($contentProperties, $model);
-            $contentProperties = $tmpArray;
+        // choose, which uri to use: option over helper default over view value
+        $uri = (isset($this->resourceUri))           ? $this->resourceUri : null;
+        $uri = (isset($options['selectedResource'])) ? (string) $options['selectedResource'] : $uri;
+        $uri = (isset($options['uri']))              ? (string) $options['uri'] : $uri;
+        $uri = Erfurt_Uri::getFromQnameOrUri($uri, $model);
+
+        // choose, which properties to use (todo: allow multple properties)
+        $contentProperties = (isset($options['property'])) ? array( $options['property']) : null;
+        $contentProperties = (!$contentProperties) ? $this->contentProperties : $contentProperties;
+
+        foreach ($contentProperties as $key => $value) {
+            try {
+                $validatedValue = Erfurt_Uri::getFromQnameOrUri($value, $model);
+                $contentProperties[$key] = $validatedValue;
+            } catch (Exception $e) {
+                unset($contentProperties[$key]);
+            }
         }
+
+        // create description from resource URI
+        $resource     = new OntoWiki_Resource($uri, $model);
+        $description  = $resource->getDescription();
+        $description  = $description[$uri];
 
         // select the main property from existing ones
         $mainProperty = null; // the URI of the main content property
         foreach ($contentProperties as $contentProperty) {
-            if (isset($desc[$contentProperty])) {
+            if (isset($description[$contentProperty])) {
                 $mainProperty = $contentProperty;
                 break;
             }
@@ -79,7 +93,7 @@ class Site_View_Helper_LiteralTag extends Zend_View_Helper_Abstract
         // filter and render the (first) literal value of the main property
         // TODO: striptags and tidying as extension
         if ($mainProperty) {
-            $firstLiteral = $desc[$mainProperty][0];
+            $firstLiteral = $description[$mainProperty][0];
             $content = $firstLiteral['value'];
 
             // filter by using available extensions
@@ -95,6 +109,8 @@ class Site_View_Helper_LiteralTag extends Zend_View_Helper_Abstract
 
             $curie = $this->view->curie($mainProperty);
             return "$prefix<$tag class='$class' property='$curie'>$iprefix$content$isuffix</$tag>$suffix";
+        } else {
+            return '';
         }
 
     }
@@ -105,6 +121,7 @@ class Site_View_Helper_LiteralTag extends Zend_View_Helper_Abstract
     public function setView(Zend_View_Interface $view)
     {
         $this->view = $view;
+        $this->resourceUri  = (string) $view->resourceUri;
     }
 
 }
