04a72bf85291d46835d1d6c4787ca7d056e7dcdb
Tim Ermilov <yamalight@gmail.com>
2012-03-08 13:57:24 +0100

    put back tabs stuff, add resource/list menu, fix some stuff

--- application/controllers/ResourceController.php
+++ application/controllers/ResourceController.php
@@ -262,9 +262,6 @@ class ResourceController extends OntoWiki_Controller_Base {
         $url = new OntoWiki_Url();
         $this->view->redirectUrl = (string)$url;
 
-		// disable tabs
-		OntoWiki_Navigation::disableNavigation();
-
         $this->addModuleContext('main.window.list');
         $this->addModuleContext('main.window.instances');
     }
