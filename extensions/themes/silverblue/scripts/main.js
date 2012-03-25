6c77323bf3dcab11518850a80d83428a25f692b5
Clemens Hoffmann <cannelony@googlemail.com>
2012-03-25 16:58:09 +0200

    strip whitespaces

--- extensions/themes/silverblue/scripts/main.js
+++ extensions/themes/silverblue/scripts/main.js
@@ -39,16 +39,16 @@ $(document).ready(function() {
                   'constructor'
                 ],
                 dontEnumsLength = dontEnums.length
-    
+
             return function (obj) {
                 if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object')
-        
+
                 var result = []
-    
+
                 for (var prop in obj) {
                     if (hasOwnProperty.call(obj, prop)) result.push(prop)
                 }
-    
+
                 if (hasDontEnumBug) {
                     for (var i=0; i < dontEnumsLength; i++) {
                         if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i])
