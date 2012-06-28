9459c6e33de16e33540c5ec6ed9c1c410d5f308f
Philipp Frischmuth <philipp@frischmuth24.de>
2012-06-28 19:52:06 +0200

    Fixes OntoWiki_NavigationTest

--- application/tests/OntoWiki/NavigationTest.php
+++ application/tests/OntoWiki/NavigationTest.php
@@ -1,182 +1,185 @@
 <?php
 
-require_once dirname (__FILE__) .'/../TestHelper.php';
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
 
 class OntoWiki_NavigationTest extends PHPUnit_Framework_TestCase
 {
-    protected $_ontoWikiNavigation;
+    private $_ontoWikiNavigation = null;
     
     public function setUp()
     {
-        $this->_ontoWikiNavigation = new OntoWiki_Navigation ();
+        $this->_ontoWikiNavigation = new OntoWiki_Navigation();
     }
-    
-    public function tearDown() 
-    {
         
-    }
-    
-    /**
-     * Test cases for
-     * OntoWiki_Navigation -> register ---------------------------------
-     */
-    
     public function testRegisterKeyEmptyOptions()
     {
-        $this->_ontoWikiNavigation->register (
-            'foo', array (), false
-        );
+        $this->_ontoWikiNavigation->register('foo', array (), false);
         
-        $check = array ( 'foo' => array(
-            'route' => NULL, 'controller' => NULL, 'action' => NULL, 'name' => 'foo', 'active' => 'active'
+        $check = array('foo' => array(
+            'route'      => null, 
+            'controller' => null, 
+            'action'     => null, 
+            'name'       => 'foo', 
+            'active'     => 'active'
         ));
         
-        $this->assertEquals ( $check, $this->_ontoWikiNavigation->getNavigation () );
+        $this->assertEquals($check, $this->_ontoWikiNavigation->getNavigation());
     }
     
     public function testRegisterKeyDefaultOptions()
     {
-        $this->_ontoWikiNavigation->register (
-            'foo', array('route' => NULL, 'controller' => NULL, 'action' => NULL, 'name' => 'foo'), false
+        $this->_ontoWikiNavigation->register(
+            'foo', 
+            array(
+                'route'      => null, 
+                'controller' => null, 
+                'action'     => null, 
+                'name'       => 'foo'
+            ), 
+            false
         );
         
-        $check = array ( 'foo' => array(
-            'route' => NULL, 'controller' => NULL, 'action' => NULL, 'name' => 'foo', 'active' => 'active'
+        $check = array('foo' => array(
+            'route'      => null, 
+            'controller' => null, 
+            'action'     => null, 
+            'name'       => 'foo', 
+            'active'     => 'active'
         ));
         
-        $this->assertEquals ( $check, $this->_ontoWikiNavigation->getNavigation () );
+        $this->assertEquals($check, $this->_ontoWikiNavigation->getNavigation());
     }
     
+    /**
+     * @expectedException OntoWiki_Exception
+     */
     public function testRegisterAlreadyUsedKeyDefaultOptions()
     {
-        $this->_ontoWikiNavigation->register (
-            'foo', array('route' => NULL, 'controller' => NULL, 'action' => NULL, 'name' => 'foo'), false
+        $this->_ontoWikiNavigation->register(
+            'foo', 
+            array(
+                'route'      => null, 
+                'controller' => null, 
+                'action'     => null, 
+                'name'       => 'foo'
+            ), 
+            false
         );
         
-        try {
-            $this->_ontoWikiNavigation->register (
-                'foo', array('route' => NULL, 'controller' => NULL, 'action' => NULL, 'name' => 'foo'), false
-            );
-        } catch ( Exception $e ) { 
-            // Everything is fine
-            return;
-        }
-        
-        throw new Exception ('OntoWiki_Navigation->register have to trow an exception here!');
+        $this->_ontoWikiNavigation->register(
+            'foo', 
+            array(
+                'route'      => null, 
+                'controller' => null, 
+                'action'     => null, 
+                'name'       => 'foo'
+            ), 
+            false
+        );
     }
     
+    /**
+     * @expectedException OntoWiki_Exception
+     */
     public function testRegisterAlreadyUsedKeyFilledOptions()
     {
-        $this->_ontoWikiNavigation->register (
-            'foo', array('route' => 'foo', 'controller' => 'bar', 'action' => 'bar', 'name' => 'foo'), false
+        $this->_ontoWikiNavigation->register(
+            'foo', 
+            array(
+                'route'      => 'foo', 
+                'controller' => 'bar', 
+                'action'     => 'bar', 
+                'name'       => 'foo'
+            ), 
+            false
         );
         
-        try {
-            $this->_ontoWikiNavigation->register (
-                'foo', array('route' => 'foo', 'controller' => 'bar', 'action' => 'bar', 'name' => 'foo'), false
-            );
-        } catch ( Exception $e ) { 
-            // Everything is fine
-            return;
-        }
-        
-        throw new Exception ('OntoWiki_Navigation->register have to trow an exception here!');
+        $this->_ontoWikiNavigation->register(
+            'foo', 
+            array(
+                'route'      => 'foo', 
+                'controller' => 'bar', 
+                'action'     => 'bar', 
+                'name'       => 'foo'
+            ), 
+            false
+        );
     }
     
+    /**
+     * @expectedException OntoWiki_Exception
+     */
     public function testRegisterNoKey()
     {
-        try {
-            $this->_ontoWikiNavigation->register ('', array(), false);
-        } catch ( Exception $e ) { 
-            // Everything is fine
-            return;
-        }
-        
-        throw new Exception ('OntoWiki_Navigation->register have to trow an exception here!');
+        $this->_ontoWikiNavigation->register('', array(), false);
     }
     
+    /**
+     * @expectedException OntoWiki_Exception
+     */
     public function testRegisterNullKey()
     {
-        try {
-            $this->_ontoWikiNavigation->register (null, array(), false);
-        } catch ( Exception $e ) { 
-            // Everything is fine
-            return;
-        }
-        
-        throw new Exception ('OntoWiki_Navigation->register have to trow an exception here!');
+        $this->_ontoWikiNavigation->register(null, array(), false);
     }
     
+    /**
+     * @expectedException OntoWiki_Exception
+     */
     public function testRegisterNumberKey()
     {
-        try {
-            $this->_ontoWikiNavigation->register (0, array(), false);
-            return;
-        } catch ( Exception $e ) { 
-            throw $e;
-        }
+        $this->_ontoWikiNavigation->register(0, array(), false);
     }    
     
     /**
-     * Test cases for
-     * OntoWiki_Navigation -> setActive --------------------------------
+     * @expectedException OntoWiki_Exception
      */
-     
-    public function testSetActiveNoKey () 
+    public function testSetActiveNoKey() 
     {
-        try {
-            $this->_ontoWikiNavigation->setActive ('');
-        } catch ( Exception $e ) {
-            if ( 'Navigation component with key \'\' not registered.' != $e->getMessage () ) {
-                throw $e;
-            }
-        }
+        $this->_ontoWikiNavigation->setActive('');
     }
-     
-    public function testSetActiveNullKey () 
+    
+    /**
+     * @expectedException OntoWiki_Exception
+     */
+    public function testSetActiveNullKey() 
     {
-        try {
-            $this->_ontoWikiNavigation->setActive (null);
-        } catch ( Exception $e ) {
-            if ( 'Navigation component with key \'\' not registered.' != $e->getMessage () ) {
-                throw $e;
-            }
-        }
+        $this->_ontoWikiNavigation->setActive(null);
     }
-     
-    public function testSetActiveKey () 
+    
+    public function testSetActiveKey() 
     {
-        $this->_ontoWikiNavigation->register ('foo', array(), false);;
+        $this->_ontoWikiNavigation->register('foo', array(), false);
+        $this->_ontoWikiNavigation->setActive('foo');
         
+        $activeItem = $this->_ontoWikiNavigation->getActive();
+        $this->assertEquals('foo', $activeItem['name']);
+    }
+     
+    public function testNoSetActiveKeyCheckActive() 
+    {
+        $this->_ontoWikiNavigation->register('foo', array(), false);
+        
+        $activeItem = $this->_ontoWikiNavigation->getActive();
+        $this->assertEquals('foo', $activeItem['name']);
+    }
+    
+    public function testNoSetActiveKeyCheckNotActiveMultipleItems() 
+    {
+        $this->_ontoWikiNavigation->register('foo', array(), false);
+        $this->_ontoWikiNavigation->register('bar', array(), false);
+        
+        $activeItem = $this->_ontoWikiNavigation->getActive();
+        $this->assertNotEquals('bar', $activeItem['name']);
+    }
+     
+    public function testSetActiveKeyChangeActive() 
+    {
+        $this->_ontoWikiNavigation->register('foo', array(), false);
+        $this->_ontoWikiNavigation->register('oldActive', array(), false);
+        $this->_ontoWikiNavigation->setActive('oldActive');
         $this->_ontoWikiNavigation->setActive ('foo');
         
-        $active = $this->_ontoWikiNavigation->getActive();
-        if ('foo' != $active ['name']) {
-            throw new Exception ('foo is not active. Active is '. $active ['name']);
-        }
-    }
-     
-    public function testSetActiveKeyCheckActive () 
-    {
-        $this->_ontoWikiNavigation->register ('foo', array(), false);;
-        
-        $active = $this->_ontoWikiNavigation->getActive();
-        if ('foo' != $active ['name']) {
-            throw new Exception ('foo is not active. Active is '. $active ['name']);
-        }
-    }
-     
-    public function testSetActiveKeyChangeActive () 
-    {
-        $this->_ontoWikiNavigation->register ('foo', array(), false);
-        $this->_ontoWikiNavigation->register ('oldActive', array(), false);
-        $this->_ontoWikiNavigation->setActive ('oldActive');
-        
-        $this->_ontoWikiNavigation->setActive ('foo');
-        
-        $active = $this->_ontoWikiNavigation->getActive();
-        if ('foo' != $active ['name']) {
-            throw new Exception ('foo is not active. Active is '. $active ['name']);
-        }
+        $activeItem = $this->_ontoWikiNavigation->getActive();
+        $this->assertEquals('foo', $activeItem['name']);
     }
 }
