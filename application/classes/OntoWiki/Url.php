5a157cb6c17a8fa1f0afabfbeeb340b156038fb6
Jonas Brekle <jonas.brekle@gmail.com>
2012-07-13 10:16:07 +0200

    refactor OntoWiki_Url for unittestability

--- application/classes/OntoWiki/Url.php
+++ application/classes/OntoWiki/Url.php
@@ -58,9 +58,14 @@ class OntoWiki_Url
     protected $_useSefUrls = true;
     
     /**
+     * 
+     */
+    protected $_base = null;
+
+    /**
      * Constructor
      */
-    public function __construct(array $options = array(), $paramsToKeep = null, $paramsToExclude = null)
+    public function __construct(array $options = array(), $paramsToKeep = null, $paramsToExclude = null, $base = null)
     {
         $this->_request    = Zend_Controller_Front::getInstance()->getRequest();
         $defaultAction     = Zend_Controller_Front::getInstance()->getDefaultAction();
@@ -71,6 +76,14 @@ class OntoWiki_Url
         if (!isset($options['route']) && !isset($options['controller']) && !isset($options['action'])){
             $options['controller'] = $this->_request->getControllerName();
             $options['action'] = $this->_request->getActionName();
+        }
+        
+        if($base != null && is_string($base)){
+            $this->_base = $base;
+        } else if(isset(OntoWiki::getInstance()->config->urlBase)){
+            $this->_base = OntoWiki::getInstance()->config->urlBase;
+        } else {
+            $this->_base = "http://ns.aksw.org/undefined/";
         }
         
         // keep parameters
@@ -138,6 +151,15 @@ class OntoWiki_Url
         unset($this->_params['module']);
         unset($this->_params['controller']);
         unset($this->_params['action']);
+    }
+    
+    /**
+     * set base url (for unittests)
+     * @param string $base
+     */
+    public function setBase($base) 
+    { 
+        $this->_base = $base;
     }
     
     /**
@@ -262,10 +284,9 @@ class OntoWiki_Url
      */
     protected function _buildQuery()
     {
-        $base = OntoWiki::getInstance()->config->urlBase;
         
         $event = new Erfurt_Event('onBuildUrl');
-        $event->base = $base;
+        $event->base = $this->_base;
         $event->route = $this->_route;
         $event->controller = $this->_controller;
         $event->action     = $this->_action;
@@ -276,7 +297,6 @@ class OntoWiki_Url
             if ($urlCreated && isset($event->url)) {
                 return $event->url;
             } else {
-                $base = $event->base;
                 $this->_params = $event->params;
                 $this->_controller = $event->controller;
                 $this->_action     = $event->action;
@@ -333,7 +353,7 @@ class OntoWiki_Url
         // HACK:
         $this->_useSefUrls = true;
         
-        return $base . ltrim($url, '/');
+        return $this->_base . ltrim($url, '/');
     }
 }
 
