de41b4dc0ba9398f725afff94256d85ba95a44b1
Philipp Frischmuth <philipp@frischmuth24.de>
2012-08-21 16:49:12 +0200

    Add ant package to vagrant developer box puppet

--- application/scripts/configuration/modules/bootstrap/manifests/init.pp
+++ application/scripts/configuration/modules/bootstrap/manifests/init.pp
@@ -5,14 +5,14 @@ class bootstrap {
     #group { puppet:
     #    ensure => present,
     #}
-    
+
     # update apt
     exec { "apt-update":
         command => "/usr/bin/apt-get update",
     }
-    
+
     # install some useful applications
-    package { ["htop", "vim", "unzip", "graphviz", "git", "curl"]:
+    package { ["htop", "vim", "unzip", "graphviz", "git", "curl", "openjdk-6-jdk", "ant"]:
         ensure => installed,
         require => Exec["apt-update"]
     }
