e9a86241973d844d68fa6d88d62bf5ebe8ecbad5
Philipp Frischmuth <philipp@frischmuth24.de>
2012-08-27 18:17:08 +0200

    Add raptor and zsh packages, use seebis zsh repo

--- application/scripts/configuration/modules/bootstrap/manifests/init.pp
+++ application/scripts/configuration/modules/bootstrap/manifests/init.pp
@@ -12,8 +12,24 @@ class bootstrap {
     }
 
     # install some useful applications
-    package { ["htop", "vim", "unzip", "graphviz", "git", "curl", "openjdk-6-jdk", "ant"]:
+    package { ["htop", "vim", "unzip", "graphviz", "git", "curl", "openjdk-6-jdk", "ant", "zsh", "raptor2-utils"]:
         ensure => installed,
         require => Exec["apt-update"]
+    }
+
+    exec { "chsh-to-zsh":
+        command => "/usr/bin/chsh -s /usr/bin/zsh vagrant",
+        require => Package["zsh"]
+    }
+
+    exec { "clone-seebi-zshrc":
+        command => "/usr/bin/git clone git://github.com/seebi/zshrc.git && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git zshrc/syntax-highlighting && git clone git://github.com/joelthelion/autojump.git zshrc/autojump && git clone git://github.com/seebi/dircolors-solarized.git zshrc/dircolors-solarized && cd zshrc/autojump && git checkout release-v16 && cd /home/vagrant/zshrc && export HOME=/home/vagrant && make install-core",
+        cwd => "/home/vagrant",
+        path => "/bin:/usr/bin",
+        creates => "/home/vagrant/zshrc",
+        user => "vagrant",
+        provider => "shell",
+        logoutput => "on_failure",
+        require => [Package["zsh"], Package["git"]]
     }
 }
