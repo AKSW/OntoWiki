0ac53438d64885bbd1aa28637c992da4292fbad9
Natanael Arndt <arndtn@gmail.com>
2012-10-26 02:03:50 +0200

    check resources for null to avoid error in loop

--- application/classes/OntoWiki/Model/TitleHelper.php
+++ application/classes/OntoWiki/Model/TitleHelper.php
@@ -428,6 +428,9 @@ class OntoWiki_Model_TitleHelper
         $currentQuery = null;
         $queries = array();
         $select = 'SELECT DISTINCT ?property ?value';
+        if ($this->_resources === null) {
+            return array();
+        }
         foreach ($this->_resources as $resourceUri) {
             $where = 'WHERE {'
                    . $this->_getTitleWhere($resourceUri)
