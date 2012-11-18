41bb805e01f8f7b8b75a7637fdc946dfa3817be0
Natanael Arndt <arndtn@gmail.com>
2012-11-18 16:07:14 +0100

    apply coding standarts to ResourceUriGenerator

--- extensions/resourcecreationuri/classes/ResourceUriGenerator.php
+++ extensions/resourcecreationuri/classes/ResourceUriGenerator.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* This file is part of the {@link http://ontowiki.net OntoWiki} project.
-*
-* @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
-* @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
-*/
+ * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ *
+ * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
 
 /**
  *
@@ -13,13 +13,13 @@
  * @author criess
  *
  */
-class ResourceUriGenerator {
-
+class ResourceUriGenerator
+{
     /**
      * @var Array that holds multibyte and special chars in UTF-8 to uri compatible chars.
      *      All other non-alphanumeric will be deleted
      */
-    private $charTable = array (
+    private $_charTable = array (
         'Ä'     => 'Ae' ,
         'ä'     => 'ae' ,
         'Ü'     => 'Ue' ,
@@ -30,57 +30,57 @@ class ResourceUriGenerator {
         PHP_EOL => '_'  ,
         ':'     => '_'  ,
     );
-    
+
     /**
      * 
      * Enter description here ...
      * @var array
      */
     private $_defaultConfig = array();
-    
+
     /**
      * 
      * Enter description here ...
      * @var OntoWiki (Application)
      */
     private $_owApp = null;
-    
+
     /**
      * 
      * Enter description here ...
      * @var Zend_Config
      */
     private $_config = null;
-    
+
     /**
-     * 
+     *
      * Enter description here ...
      * @var Erfurt_Rdf_Model
      */
     private $_model = null;
-    
+
     /**
-     * 
+     *
      * Enter description here ...
      * @var string
      */
     const FORMAT_RDFPHP = 'rdfphp';
-    
+
     /**
-     * 
+     *
      * Enter description here ...
      * @var string
      */
     const FORMAT_SPARQL = 'sparql';
-    
+
     /**
      * constructor
      * @param $resourceUri
      * @param $defaultModel
      * @param $configPath
      */
-    public function __construct($defaultModel = null, $configPath = null, $ow = null) {
-
+    public function __construct($defaultModel = null, $configPath = null, $ow = null)
+    {
         // check for ontowiki application
         if ( $ow === null || !($ow instanceof OntoWiki) ) {
             $this->_owApp = OntoWiki::getInstance();
@@ -91,7 +91,7 @@ class ResourceUriGenerator {
         //get config
         $config = $this->_owApp->extensionManager->getExtensionConfig("resourcecreationuri");
         $this->_config = $config->private;
-        
+
         // check for defaultModel to work on
         if ($defaultModel === null && $this->_owApp->selectedModel !== null) {
             $this->_model = $this->_owApp->selectedModel;
@@ -103,32 +103,33 @@ class ResourceUriGenerator {
                 throw new InvalidArgumentException('ResourceUriGenerator can\'t load model with URI: ' . $erroruri);
             }
         }
-        
     }
-    
+
     /**
      * set a new defaultmodel in which the resources should be created
      * @param $defaultModel
      */
-    public function setDefaultModel($defaultModel) {
-        
+    public function setDefaultModel($defaultModel)
+    {
+
         if ($defaultModel instanceof Erfurt_Rdf_Model) {
             $this->_model = $defaultModel;
         } else {
             $this->_model = $this->erfurt->getStore()->getModel((string) $defaultModel);
         }
     }
-    
+
     /**
      * Function generates nice URIs by certain rules (naming scheme and available resource data)
      * Two modes are possible: live from store namingly 'sparql', or with memory model in rdfphp
      * format to use 'rdfphp'.
-     * 
-     * @param string $format (see class constants for possible values) 
+     *
+     * @param string $format (see class constants for possible values)
      * @param array $data further data (in rdfphp-mode the insert statements)
      * @return string generated 'nice' URI
      */
-    public function generateUri($resourceUri, $format = self::FORMAT_SPARQL, $data = array()) {
+    public function generateUri($resourceUri, $format = self::FORMAT_SPARQL, $data = array())
+    {
         $titleHelper = new OntoWiki_Model_TitleHelper($this->_model);
 
         if (isset($this->_config->property->title)) {
@@ -149,17 +150,17 @@ class ResourceUriGenerator {
                 }
                 break;
         }
-        
+
         // check if resources with same prefix exist
         if ( ($count = $this->countUriPattern($return)) > 0) {
             $return .= '/' . $count;
         } else {
             // do nothing
         }
-        
+
         return $return;
     }
-    
+
     /**
      * 
      * Enter description here ...
@@ -177,50 +178,55 @@ class ResourceUriGenerator {
             } elseif (is_array($this->_config->property->$element->toArray())) {
                 $countDeep = 0;
                 foreach ($this->_config->property->$element->toArray() as $elementDeep) {
-                    $properties[(string) $elementDeep] = array('element' => $element, 'rank' => $countDeep++); 
+                    $properties[(string) $elementDeep] = array(
+                        'element' => $element, 'rank' => $countDeep++
+                    );
                 }
             } else {
                 // do nothing
             }
         }
-        
+
         $query = new Erfurt_Sparql_Query2();
         $sRef  = new Erfurt_Sparql_Query2_IriRef($uri);
         $pVar  = new Erfurt_Sparql_Query2_Var('p');
         $oVar  = new Erfurt_Sparql_Query2_Var('o');
-        
+
         $query->addProjectionVar($pVar);
         $query->addProjectionVar($oVar);
-        $query->addTriple($sRef,$pVar,$oVar);
+        $query->addTriple($sRef, $pVar, $oVar);
         $query->addFrom((string) $this->_model);
         $query->setLimit(100);
         $query->setDistinct(true);
-        
-        
+
         $container = new Erfurt_Sparql_Query2_ConditionalOrExpression();
         foreach ($properties as $filterProp => $element) {
             $sameTerm = new Erfurt_Sparql_Query2_sameTerm($pVar, new Erfurt_Sparql_Query2_IriRef($filterProp));
             //$filter = new Erfurt_Sparql_Query2_Filter($sameTerm);
             $container->addElement($sameTerm);
         }
-        
+
         $query->addFilter($container);
-        
-        $result = $this->_owApp->erfurt->getStore()->sparqlQuery($query,array('withImports' => true));
-        
+
+        $result = $this->_owApp->erfurt->getStore()->sparqlQuery(
+            $query, array('withImports' => true)
+        );
+
         $replacements = array();
-        
+
         foreach ($result as $row) {
-            if (array_key_exists($row['p'],$properties)) {
+            if (array_key_exists($row['p'], $properties)) {
                 $titleHelper->addResource($row['p']);
                 if (Erfurt_Uri::check($row['o'])) {
                     $titleHelper->addResource($row['o']);
                 }
                 if (array_key_exists($properties[$row['p']]['element'], $replacements)) {
-                    if ((int)$properties[$row['p']]['rank'] < (int)$replacements[$properties[$row['p']]['element']]['rank']) {
+                    $newRank = (int) $properties[$row['p']]['rank'];
+                    $minRank = $replacements[$properties[$row['p']]['element']]['rank'];
+                    if ($newRank < $minRank) {
                         $replacements[$properties[$row['p']]['element']] = array(
-                    		'rank'  => $properties[$row['p']]['rank'],
-                    		'value' => $row['o'],
+                            'rank'  => $newRank,
+                            'value' => $row['o'],
                             'key'   => $row['p']
                         );
                     } else {
@@ -228,31 +234,31 @@ class ResourceUriGenerator {
                     }
                 } else {
                     $replacements[$properties[$row['p']]['element']] = array(
-                    	'rank'  => $properties[$row['p']]['rank'],
-                    	'value' => $row['o'],
+                        'rank'  => $properties[$row['p']]['rank'],
+                        'value' => $row['o'],
                         'key'   => $row['p']
                     );
                 }
             }
         }
-        
+
         $localName = '';
 
         foreach ($schema as $element) {
-            if (array_key_exists($element,$replacements)) {
+            if (array_key_exists($element, $replacements)) {
                 if (Erfurt_Uri::check($replacements[$element]['value'])) {
                     $val = $titleHelper->getTitle($replacements[$element]['value']);
                 } else {
                     $val = $replacements[$element]['value'];
                 }
                 $val = $this->convertChars($val);
-                
+
                 $key = $this->convertChars($titleHelper->getTitle($replacements[$element]['key']));
-                
+
                 $localName .=  $key . '/' . $val . '/';
             }
         }
-        
+
         // no meaningful localname created falback to old uri (TODO or md5 a new one?)
         if ($localName === '') {
             return $uri;
@@ -260,7 +266,7 @@ class ResourceUriGenerator {
         }
 
         $base = '';
-        
+
         if ($this->_model !== null && $this->_model->getBaseIri() !== '') {
             $base = $this->_model->getBaseIri();
             if ($base[strlen($base)-1] !== '#' && $base[strlen($base)-1] !== '/') {
@@ -268,7 +274,7 @@ class ResourceUriGenerator {
             }
         } else {
             $count = 0;
-            foreach (explode('/',$uri) as $element) {
+            foreach (explode('/', $uri) as $element) {
                 if ($count > 2) {
                     break;
                 } else {
@@ -277,9 +283,9 @@ class ResourceUriGenerator {
                 }
             }
         }
-        
+
         return $base . $localName;
-        
+
     }
 
     /**
@@ -294,7 +300,7 @@ class ResourceUriGenerator {
         // prepare TitleHelper by adding all possible resources
         foreach ($newInstance as $prop => $object) {
             $titleHelper->addResource($prop);
-            foreach($object as $value) {
+            foreach ($object as $value) {
                 if ($value['type'] === 'uri') {
                     $titleHelper->addResource($value['value']);
                 }
@@ -304,7 +310,7 @@ class ResourceUriGenerator {
         $nameParts = $this->loadNamingSchema($uri);
         $uriParts = array();
 
-        foreach($nameParts as $part) {
+        foreach ($nameParts as $part) {
             if (is_string($this->_config->property->$part)) {
                 $partProperties = array($this->_config->property->$part);
             } else if (is_array($this->_config->property->$part->toArray())) {
@@ -375,14 +381,14 @@ class ResourceUriGenerator {
     private function loadNamingSchema($resourceUri, $typeHint = null)
     {
         if ( $this->_config->fromModel ) {
-        
+
             // TODO Needs to be completly rewritten.
             /*
             $query          = new Erfurt_Sparql_Query2();
             $schemaVar      = new Erfurt_Sparql_Query2_Var('schema');
-            
+
             $subjectArray   = array_keys($this->insertData);
-            
+
             // Test if exists at least one subject and if this first subject has a type statement
             if ( sizeof($subjectArray) > 0 &&
                  array_key_exists($this->_config->property->type, $subjectArray)
@@ -390,30 +396,29 @@ class ResourceUriGenerator {
                 $subjectUri     = current($subjectArray);
                 $typeArray      = current($this->insertData[$subjectUri][$this->_config->property->type]);
                 $type           = $typeArray['value'];
-            
+
                 $query->addTriple(
                     new Erfurt_Sparql_Query2_IriRef($type),
                     new Erfurt_Sparql_Query2_IriRef($this->_config->namingSchemeProperty),
                     $schemaVar
                 );
-                
+
                 $query->setDistinct(true);
-                
+
                 $result = $this->_model->sparqlQuery($query);
-                
+
                 if ( !empty($result['results']['bindings']) ) {
                     $schema = current($result);
                     return explode('/',$schema['schema']['value']);
                 }
             }
             */
-            
-        }       
-        
-        return explode('/',$this->_config->defaultNamingScheme);
-        
+
+        }
+
+        return explode('/', $this->_config->defaultNamingScheme);
     }
-    
+
     /**
      * Method to convert chars in a string to uri compatible
      * @param $str any string
@@ -421,14 +426,14 @@ class ResourceUriGenerator {
      */
     private function convertChars($str)
     {
-        foreach ($this->charTable as $key => $value) {
+        foreach ($this->_charTable as $key => $value) {
             $str = str_replace($key, $value, $str);
         }
-        $str= preg_replace('/[^a-z0-9_]+/i','',$str);
+        $str = preg_replace('/[^a-z0-9_]+/i', '', $str);
         //$str = substr($str,0,32);
         return $str;
     }
-    
+
     /**
      * Method that counts already existing distinct datasets for given uri
      * 
@@ -439,52 +444,51 @@ class ResourceUriGenerator {
     {
         $query = new Erfurt_Sparql_Query2();
         $query->setDistinct(true);
-        
+
         $unions = new Erfurt_Sparql_Query2_GroupOrUnionGraphPattern();
-        
+
         $subjectVar = new Erfurt_Sparql_Query2_Var('s');
         $query->addProjectionVar($subjectVar);
-        
+
         // create six temporary vars (not selected in query)
         $tempVars = array();
         for ($i = 0;$i < 6; $i++) {
             $tempVars[] = new Erfurt_Sparql_Query2_Var('var' . $i);
         }
-        
+
         $singlePattern = new Erfurt_Sparql_Query2_GroupGraphPattern();
-        $singlePattern->addTriple($subjectVar,$tempVars[0],$tempVars[1]);
+        $singlePattern->addTriple($subjectVar, $tempVars[0], $tempVars[1]);
         $unions->addElement($singlePattern);
-        
+
         $singlePattern = new Erfurt_Sparql_Query2_GroupGraphPattern();
-        $singlePattern->addTriple($tempVars[2],$subjectVar,$tempVars[3]);
+        $singlePattern->addTriple($tempVars[2], $subjectVar, $tempVars[3]);
         $unions->addElement($singlePattern);
-        
+
         $singlePattern = new Erfurt_Sparql_Query2_GroupGraphPattern();
-        $singlePattern->addTriple($tempVars[4],$tempVars[5],$subjectVar);
+        $singlePattern->addTriple($tempVars[4], $tempVars[5], $subjectVar);
         $unions->addElement($singlePattern);
-        
+
         $query->getWhere()->addElement($unions);
 
         $filter = new Erfurt_Sparql_Query2_ConditionalOrExpression();
 
         $filter->addElement(
             new Erfurt_Sparql_Query2_Regex(
-                    $subjectVar,
-                    new Erfurt_Sparql_Query2_RDFLiteral('^' . $uri),
-                    new Erfurt_Sparql_Query2_RDFLiteral('i')
+                $subjectVar,
+                new Erfurt_Sparql_Query2_RDFLiteral('^' . $uri),
+                new Erfurt_Sparql_Query2_RDFLiteral('i')
             )
         );
 
         $query->addFilter($filter);
-        
+
         $result = $this->_owApp->erfurt->getStore()->countWhereMatches(
             $this->_model->getModelIri(),
             $query->getWhere(),
             's',
             true
         );
-        
+
         return $result;
     }
-    
 }
