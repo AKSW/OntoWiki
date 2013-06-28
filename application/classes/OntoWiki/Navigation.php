c5b745f4dec9604e28cbc7130ea26272deb5478d
Tim Ermilov <yamalight@gmail.com>
2013-06-28 15:06:20 +0200

    do not append active/active params to URL in tabs, closes #232

--- application/classes/OntoWiki/Navigation.php
+++ application/classes/OntoWiki/Navigation.php
@@ -314,9 +314,6 @@ class OntoWiki_Navigation
         $current  = $this->_navigation[$elementKey];
         $hasRoute = false;
 
-        $currentController = $request->getControllerName();
-        $currentAction     = $request->getActionName();
-
         if (isset($current['route'])) {
             if ($router->hasRoute($current['route'])) {
                 $route    = $router->getRoute($current['route']);
@@ -337,7 +334,7 @@ class OntoWiki_Navigation
             $url = new OntoWiki_Url(array('controller' => $controller, 'action' => $action), $this->_keepParams);
         }
         foreach ($current as $key => $value) {
-            if ($key != 'route' && $key != 'controller' && $key != 'action' && $key != 'priority' && $key != 'name') {
+            if ($key != 'route' && $key != 'controller' && $key != 'action' && $key != 'priority' && $key != 'name' && $key != 'active') {
                 $url->setParam($key, $value);
             }
         }
