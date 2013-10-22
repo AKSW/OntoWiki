1a491707632b14aa64da4ef58b833eefa99879f8
Sebastian Tramp <mail@sebastian.tramp.name>
2013-10-22 15:34:50 +0200

    doc and cs issues

--- extensions/mail/MailPlugin.php
+++ extensions/mail/MailPlugin.php
@@ -2,35 +2,27 @@
 /**
  * This file is part of the {@link http://ontowiki.net OntoWiki} project.
  *
- * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
+ * @copyright Copyright (c) 2013, {@link http://aksw.org AKSW}
  * @license   http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  */
-
-require_once 'OntoWiki/Plugin.php';
-require_once 'Zend/Mail.php';
 
 /**
  * This class includes a plugin to send mails via an event from different places
  * within ontowiki.
  *
- * Long description for class (if any) ...
- *
  * @category   OntoWiki
- * @package    Extensions_Sendmail
- * @copyright  Copyright (c) 2012, {@link http://aksw.org AKSW}
- * @author     Christian Maier <christianmaier83@gmail.com>
- * @author     Michael Niederstätter <michael.niederstaetter@gmail.com>
- * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @package    Extensions_Mail
  */
 class MailPlugin extends OntoWiki_Plugin
 {
     public function onAnnounceWorker($event)
     {
+        // key name, class file, class name, config
         $event->registry->registerJob(
-            "testMail",                                                         //  job key name
-            "extensions/mail/jobs/Mail.php",                                    //  job class file
-            "Mail_Job_Mail",                                                    //  job class name
-            $this->_privateConfig->smtp->toArray()                              //  extension configuration
+            'testMail',
+            'extensions/mail/jobs/Mail.php',
+            'Mail_Job_Mail',
+            $this->_privateConfig->smtp->toArray()
         );
     }
 }
--- extensions/mail/jobs/Mail.php
+++ extensions/mail/jobs/Mail.php
@@ -1,26 +1,41 @@
 <?php
-class Mail_Job_Mail extends Erfurt_Worker_Job_Abstract{
+/**
+ * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ *
+ * @copyright Copyright (c) 2013, {@link http://aksw.org AKSW}
+ * @license   http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
 
+/**
+ * This class includes a mail send job
+ *
+ * @category   OntoWiki
+ * @package    Extensions_Mail
+ */
+class Mail_Job_Mail extends Erfurt_Worker_Job_Abstract
+{
     private $_owApp     = null;
     private $_config    = null;
     private $_transport = null;
 
-    public function __init(){
+    public function __init()
+    {
         $this->_owApp   = OntoWiki::getInstance();
         $this->_config  = $this->_owApp->config;
     }
 
-    public function run($workload){
+    public function run($workload)
+    {
         $smtpServer = $this->options['server'];
         $config = array();
-        if ($this->options['auth']){
+        if ($this->options['auth']) {
             $config['auth']      = $this->options['auth'];
             $config['username']  = $this->options['username'];
             $config['password']  = $this->options['password'];
         }
         $this->_transport = new Zend_Mail_Transport_Smtp($smtpServer, $config);
 
-        if (is_object($workload)){
+        if (is_object($workload)) {
             $mail = new Zend_Mail();
             $mail->setDefaultTransport($this->_transport);
             $mail->addTo($workload->receiver);
