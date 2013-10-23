4b5ae817ecb1fedc09b32f046263bc8f7a970d50
Christian Würker <kriss@ceusmedia.de>
2013-10-23 16:11:42 +0200

    Reorganize shell scripts to avoid build failure.

--- application/shell.worker.php
+++ application/shell.worker.php
@@ -15,9 +15,6 @@
  * @codeCoverageIgnore
  */
 
-/* Profiling */
-define('REQUEST_START', microtime(true));
-
 /*
  * error handling for the very first includes etc.
  * http://stackoverflow.com/questions/1241728/
@@ -30,7 +27,6 @@ function errorHandler ($errno, $errstr, $errfile, $errline, array $errcontext)
     }
     throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 }
-set_error_handler('errorHandler');
 
 /*
  * method to get evironment variables which are prefixed with "REDIRECT_"
@@ -53,106 +49,114 @@ function getEnvVar ($key)
     return null;
 }
 
-/**
- * Bootstrap constants
- * @since 0.9.5
- */
-if (!defined('__DIR__')) {
-    define('__DIR__', dirname(__FILE__));
-} // fix for PHP < 5.3.0
-define('BOOTSTRAP_FILE', basename(__FILE__));
-define('ONTOWIKI_ROOT', rtrim(dirname(__DIR__), '/\\') . DIRECTORY_SEPARATOR);
-define('APPLICATION_PATH', ONTOWIKI_ROOT . 'application'.DIRECTORY_SEPARATOR);
-define('CACHE_PATH', ONTOWIKI_ROOT . 'cache'.DIRECTORY_SEPARATOR);
+function initApp(){
+    /* Profiling */
+    define('REQUEST_START', microtime(true));
 
-/**
- * Old constants for < 0.9.5 backward compatibility
- * @deprecated 0.9.5
- */
-define('_OWBOOT', BOOTSTRAP_FILE);
-define('_OWROOT', ONTOWIKI_ROOT);
-define('OW_SHOW_MAX', 5);
+    set_error_handler('errorHandler');
+       /**
+     * Bootstrap constants
+     * @since 0.9.5
+     */
+    if (!defined('__DIR__')) {
+        define('__DIR__', dirname(__FILE__));
+    } // fix for PHP < 5.3.0
+    define('BOOTSTRAP_FILE', basename(__FILE__));
+    define('ONTOWIKI_ROOT', rtrim(dirname(__DIR__), '/\\') . DIRECTORY_SEPARATOR);
+    define('APPLICATION_PATH', ONTOWIKI_ROOT . 'application'.DIRECTORY_SEPARATOR);
+    define('CACHE_PATH', ONTOWIKI_ROOT . 'cache'.DIRECTORY_SEPARATOR);
 
+    /**
+     * Old constants for < 0.9.5 backward compatibility
+     * @deprecated 0.9.5
+     */
+    define('_OWBOOT', BOOTSTRAP_FILE);
+    define('_OWROOT', ONTOWIKI_ROOT);
+    define('OW_SHOW_MAX', 5);
 
-// PHP environment settings
-ini_set('max_execution_time', 240);
+    // PHP environment settings
+    ini_set('max_execution_time', 240);
 
-if ((int)substr(ini_get('memory_limit'), 0, -1) < 256) {
-    ini_set('memory_limit', '256M');
+    if ((int)substr(ini_get('memory_limit'), 0, -1) < 256) {
+        ini_set('memory_limit', '256M');
+    }
+
+    /*
+     * include path preparation
+     */
+    // init with local path in order to prefer these over system paths
+    $includePath = ONTOWIKI_ROOT . 'libraries/' . PATH_SEPARATOR;
+    // append local Erfurt include path
+    if (file_exists(ONTOWIKI_ROOT . 'libraries/Erfurt/Erfurt/App.php')) {
+        $includePath .= ONTOWIKI_ROOT . 'libraries/Erfurt/' . PATH_SEPARATOR;
+    } else if (file_exists(ONTOWIKI_ROOT . 'libraries/Erfurt/library/Erfurt/App.php')) {
+        $includePath .= ONTOWIKI_ROOT . 'libraries/Erfurt/library' . PATH_SEPARATOR;
+    }
+    // append system include paths
+    $includePath .= get_include_path() . PATH_SEPARATOR;
+    // set the include path
+    set_include_path($includePath);
+
+    // use default timezone from php.ini or let PHP guess it
+    date_default_timezone_set(@date_default_timezone_get());
+
+    // determine wheter rewrite engine works
+    // and redirect to a URL that doesn't need rewriting
+    // TODO: check for AllowOverride All
+    $rewriteEngineOn = false;
+    define('ONTOWIKI_REWRITE', $rewriteEngineOn);
+
+    /** check/include Zend_Application */
+    try {
+        // use include, so we can catch it with the error handler
+        require_once 'Zend/Application.php';
+    } catch (Exception $e) {
+        header('HTTP/1.1 500 Internal Server Error');
+        echo 'Fatal Error: Could not load Zend library.<br />' . PHP_EOL
+             . 'Maybe you need to install it with apt-get or with "make zend"?' . PHP_EOL;
+        exit;
+    }
+
+    // create application
+    $application = new Zend_Application(
+        'default',
+        ONTOWIKI_ROOT . 'application/config/application.ini'
+    );
+
+    /** check/include OntoWiki */
+    try {
+        // use include, so we can catch it with the error handler
+        require_once 'OntoWiki.php';
+    } catch (Exception $e) {
+        print('Fatal Error: Could not load the OntoWiki Application Framework classes.' . PHP_EOL);
+        print('Your installation directory seems to be screwed.' . PHP_EOL);
+        exit;
+    }
+
+    /* check/include Erfurt_App */
+    try {
+        // use include, so we can catch it with the error handler
+        require_once 'Erfurt/App.php';
+    } catch (Exception $e) {
+        print('Fatal Error: Could not load the Erfurt Framework classes.' . PHP_EOL);
+        print('Maybe you should install it with apt-get or with "make deploy"?' . PHP_EOL);
+        exit;
+    }
+
+    // restore old error handler
+    restore_error_handler();
+
+    // bootstrap
+    try {
+        $application->bootstrap();
+    } catch (Exception $e) {
+        print('Error on bootstrapping application: ' . $e->getMessage() . PHP_EOL);
+        exit;
+    }
+    return $application;
 }
 
-/*
- * include path preparation
- */
-// init with local path in order to prefer these over system paths
-$includePath = ONTOWIKI_ROOT . 'libraries/' . PATH_SEPARATOR;
-// append local Erfurt include path
-if (file_exists(ONTOWIKI_ROOT . 'libraries/Erfurt/Erfurt/App.php')) {
-    $includePath .= ONTOWIKI_ROOT . 'libraries/Erfurt/' . PATH_SEPARATOR;
-} else if (file_exists(ONTOWIKI_ROOT . 'libraries/Erfurt/library/Erfurt/App.php')) {
-    $includePath .= ONTOWIKI_ROOT . 'libraries/Erfurt/library' . PATH_SEPARATOR;
-}
-// append system include paths
-$includePath .= get_include_path() . PATH_SEPARATOR;
-// set the include path
-set_include_path($includePath);
-
-// use default timezone from php.ini or let PHP guess it
-date_default_timezone_set(@date_default_timezone_get());
-
-// determine wheter rewrite engine works
-// and redirect to a URL that doesn't need rewriting
-// TODO: check for AllowOverride All
-$rewriteEngineOn = false;
-define('ONTOWIKI_REWRITE', $rewriteEngineOn);
-
-/** check/include Zend_Application */
-try {
-    // use include, so we can catch it with the error handler
-    require_once 'Zend/Application.php';
-} catch (Exception $e) {
-    header('HTTP/1.1 500 Internal Server Error');
-    echo 'Fatal Error: Could not load Zend library.<br />' . PHP_EOL
-         . 'Maybe you need to install it with apt-get or with "make zend"?' . PHP_EOL;
-    exit;
-}
-
-// create application
-$application = new Zend_Application(
-    'default',
-    ONTOWIKI_ROOT . 'application/config/application.ini'
-);
-
-/** check/include OntoWiki */
-try {
-    // use include, so we can catch it with the error handler
-    require_once 'OntoWiki.php';
-} catch (Exception $e) {
-	print('Fatal Error: Could not load the OntoWiki Application Framework classes.' . PHP_EOL);
-	print('Your installation directory seems to be screwed.' . PHP_EOL);
-	exit;
-}
-
-/* check/include Erfurt_App */
-try {
-    // use include, so we can catch it with the error handler
-    require_once 'Erfurt/App.php';
-} catch (Exception $e) {
-    print('Fatal Error: Could not load the Erfurt Framework classes.' . PHP_EOL);
-	print('Maybe you should install it with apt-get or with "make deploy"?' . PHP_EOL);
-	exit;
-}
-
-// restore old error handler
-restore_error_handler();
-
-// bootstrap
-try {
-    $application->bootstrap();
-} catch (Exception $e) {
-    print('Error on bootstrapping application: ' . $e->getMessage() . PHP_EOL);
-    exit;
-}
+$application    = initApp();
 
 $bootstrap      = $application->getBootstrap();
 $ontoWiki       = OntoWiki::getInstance();
@@ -182,8 +186,6 @@ $workerRegistry->registerJob(
     'Erfurt_Worker_TestJob',
     array()
 );
-
-
 
 //  create a new worker backend with filled worker registry
 $worker = new Erfurt_Worker_Backend($workerRegistry);
--- extensions/mail/jobs/Mail.php
+++ extensions/mail/jobs/Mail.php
@@ -23,10 +23,10 @@ class Mail_Job_Mail extends Erfurt_Worker_Job_Abstract
      *  @access     public
      *  @param      object      $workload       Object with mail sender, receiver, subject and body
      *  @return     void
-     *  @throws     InvalidArgumentException    if workload object has no sender 
-     *  @throws     InvalidArgumentException    if workload object has no receiver 
-     *  @throws     InvalidArgumentException    if workload object has no subject 
-     *  @throws     InvalidArgumentException    if workload object has no body 
+     *  @throws     InvalidArgumentException    if workload object has no sender
+     *  @throws     InvalidArgumentException    if workload object has no receiver
+     *  @throws     InvalidArgumentException    if workload object has no subject
+     *  @throws     InvalidArgumentException    if workload object has no body
      */
     public function run($workload)
     {
