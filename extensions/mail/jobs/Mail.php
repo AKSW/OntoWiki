3bda78c3caab7f4cb430bc38fc25d344508e9682
Christian Würker <kriss@ceusmedia.de>
2013-10-23 15:09:14 +0200

    Cleanup mail extension job class by removing obsolete members.

--- extensions/mail/jobs/Mail.php
+++ extensions/mail/jobs/Mail.php
@@ -2,47 +2,63 @@
 /**
  * This file is part of the {@link http://ontowiki.net OntoWiki} project.
  *
- * @copyright Copyright (c) 2013, {@link http://aksw.org AKSW}
- * @license   http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @copyright   Copyright (c) 2013, {@link http://aksw.org AKSW}
+ * @license     http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  */
 
 /**
- * This class includes a mail send job
+ * This class includes a mail send job.
  *
- * @category   OntoWiki
- * @package    Extensions_Mail
+ * @category    OntoWiki
+ * @package     Extensions_Mail
+ * @copyright   Copyright (c) 2013, {@link http://aksw.org AKSW}
+ * @author      Christian Würker <christian.wuerker@ceusmedia.de>
+ * @license     http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  */
 class Mail_Job_Mail extends Erfurt_Worker_Job_Abstract
 {
-    private $_owApp     = null;
-    private $_config    = null;
-    private $_transport = null;
-
-    public function __init()
-    {
-        $this->_owApp   = OntoWiki::getInstance();
-        $this->_config  = $this->_owApp->config;
-    }
-
+    /**
+     *  Send mail.
+     *
+     *  @access     public
+     *  @param      object      $workload       Object with mail sender, receiver, subject and body
+     *  @return     void
+     *  @throws     InvalidArgumentException    if workload object has no sender 
+     *  @throws     InvalidArgumentException    if workload object has no receiver 
+     *  @throws     InvalidArgumentException    if workload object has no subject 
+     *  @throws     InvalidArgumentException    if workload object has no body 
+     */
     public function run($workload)
     {
         $smtpServer = $this->options['server'];
-        $config = array();
+        $config     = array();
         if ($this->options['auth']) {
             $config['auth']      = $this->options['auth'];
             $config['username']  = $this->options['username'];
             $config['password']  = $this->options['password'];
         }
-        $this->_transport = new Zend_Mail_Transport_Smtp($smtpServer, $config);
+        $transport = new Zend_Mail_Transport_Smtp($smtpServer, $config);
 
         if (is_object($workload)) {
+            if (empty($workload->sender)){
+                throw new InvalidArgumentException('Workload is missing sender');
+            }
+            if (empty($workload->receiver)){
+                throw new InvalidArgumentException('Workload is missing receiver');
+            }
+            if (empty($workload->subject)){
+                throw new InvalidArgumentException('Workload is missing subject');
+            }
+            if (empty($workload->body)){
+                throw new InvalidArgumentException('Workload is missing body');
+            }
             $mail = new Zend_Mail();
-            $mail->setDefaultTransport($this->_transport);
+            $mail->setDefaultTransport($transport);
             $mail->addTo($workload->receiver);
             $mail->setSubject($workload->subject);
             $mail->setBodyText($workload->body);
             $mail->setFrom($workload->sender);
-            $mail->send($this->_transport);
+            $mail->send($transport);
             $this->logSuccess('Mail "'.$workload->subject.'" sent to '.$workload->receiver);
         }
     }
