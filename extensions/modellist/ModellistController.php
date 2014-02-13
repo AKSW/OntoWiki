b959912be29b5d28a685be436ea7b27275f6491e
Tim Ermilov <yamalight@gmail.com>
2014-02-13 12:51:43 +0100

    new modellist extension with pagination and search

---
+++ extensions/modellist/ModellistController.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ *
+ * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
+ * @license   http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+
+/**
+ * Controller for OntoWiki Modellist Extended Module
+ *
+ * @category OntoWiki
+ * @package  Extensions_Modellist
+ * @author   {@link http://codezen.ru Tim Ermilov}
+ */
+class ModellistController extends OntoWiki_Controller_Component
+{
+    /*
+     * Initializes Modellist Controller,
+     * creates class vars for current store, session and models list
+     */
+    public function init()
+    {
+        parent::init();
+        $this->session = new Zend_Session_Namespace(_OWSESSION);
+
+        $store = $this->_owApp->erfurt->getStore();
+
+        if (isset($this->session->showHiddenGraphs) && $this->session->showHiddenGraphs == true) {
+            $this->graphUris = $store->getAvailableModels(true);
+        } else {
+            $this->graphUris = $store->getAvailableModels(false);
+        }
+    }
+
+    /*
+     * The main action which is retrieved via ajax
+     */
+    public function exploreAction()
+    {
+        // disable standart navigation
+        OntoWiki::getInstance()->getNavigation()->disableNavigation();
+        // translate navigation title to selected language
+        $this->view->placeholder('main.window.title')->set($this->_owApp->translate->_('Model list'));
+        $setup = $this->_request->setup;
+
+        // get offset
+        $offset = !empty($setup['offset']) ? $setup['offset'] : 0;
+        // get limit (default to 10)
+        $limit = !empty($setup['limit']) ? $setup['limit'] : 10;
+        // search string
+        $searchString = !empty($setup['searchString']) ? $setup['searchString'] : null;
+
+        $models = array();
+        $selectedModel = $this->_owApp->selectedModel ? $this->_owApp->selectedModel->getModelIri() : null;
+
+        $lang = $this->_config->languages->locale;
+        $titleMode = $this->_privateConfig->defaults->titleMode;
+
+        if($titleMode == "titleHelper") {
+            $titleHelper = new OntoWiki_Model_TitleHelper();
+            $titleHelper->addResources(array_keys($this->graphUris));
+        }
+
+        $useGraphUriAsLink = false;
+        if (isset($this->_privateConfig->useGraphUriAsLink) && (bool)$this->_privateConfig->useGraphUriAsLink) {
+            $useGraphUriAsLink = true;
+        }
+
+        foreach ($this->graphUris as $graphUri => $true) {
+            $linkUrl = $this->_config->urlBase . 'model/select/?m=' . urlencode($graphUri);
+            if ($useGraphUriAsLink) {
+                if (isset($this->_config->vhosts)) {
+                    $vHostsArray = $this->_config->vhosts->toArray();
+                    foreach ($vHostsArray as $vHostUri) {
+                        if (strpos($graphUri, $vHostUri) !== false) {
+                            // match
+                            $linkUrl = $graphUri;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            $temp             = array();
+            $temp['url']      = $linkUrl;
+            $temp['graphUri'] = $graphUri;
+            $temp['selected'] = ($selectedModel == $graphUri ? 'selected' : '');
+
+            // use URI if no title exists
+            $label = '';
+            if($titleMode == "titleHelper") {
+                $label = $titleHelper->getTitle($graphUri, $lang);
+                $label = !empty($label) ? $label : $graphUri;
+            } else {
+                $label = $graphUri;
+            }
+            $temp['label'] = $label;
+
+            $temp['backendName'] = $true;
+
+            if($searchString == null) { // add all if no search string
+                $models[] = $temp;
+            } else {
+                // add only if matches search string
+                if(stripos($temp['label'], $searchString) !== false) {
+                    $models[] = $temp;
+                }
+            }
+        }
+
+        // build initial view
+        $this->view->entries = $models;
+
+        // set view variable for the show more button
+        $entriesCount = count($this->view->entries) - $offset;
+        if (($entriesCount > $limit)) {
+            // return only $_limit entries
+            $this->view->entries    = array_slice($this->view->entries, 0, $offset + $limit);
+            $this->view->showMeMore = true;
+        } else {
+            $this->view->showMeMore = false;
+        }
+
+        // save state to session
+        $this->savestateServer($this->view, $setup);
+    }
+
+    /*
+     * Saves current view, setup and model to state to use it on refresh
+     */
+    protected function savestateServer($view, $setup)
+    {
+        // encode setup to json
+        $setup = json_encode($setup);
+        // replace \' and \" to ' and "
+        $replaceFrom = array("\\'", '\\"');
+        $replaceTo   = array("'", '"');
+        $setup       = str_replace($replaceFrom, $replaceTo, $setup);
+
+        // save view, setup and current model to state
+        $sessionKey         = 'Modellist' . (isset($config->session->identifier) ? $config->session->identifier : '');
+        $this->stateSession = new Zend_Session_Namespace($sessionKey);
+        $this->stateSession->view  = $view->render("explore.phtml");
+        $this->stateSession->setup = $setup;
+        $this->stateSession->model = (string)$this->_owApp->selectedModel;
+    }
+}
