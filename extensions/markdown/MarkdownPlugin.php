615b87c4d6acec5f005c726cb01499c18236f761
Natanael Arndt <arndtn@gmail.com>
2014-03-06 01:21:43 +0000

    Add support for MultiMarkdown metadata
    By using the MultiMarkdown metadata format we could add multi language
    support to the sysont:markdown datatype.
    
    SeeAlso:
    https://github.com/fletcher/MultiMarkdown/wiki/MultiMarkdown-Syntax-Guide

--- extensions/markdown/MarkdownPlugin.php
+++ extensions/markdown/MarkdownPlugin.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the {@link http://ontowiki.net OntoWiki} project.
  *
- * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
+ * @copyright Copyright (c) 2014, {@link http://aksw.org AKSW}
  * @license   http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  */
 
@@ -27,15 +27,64 @@ class MarkdownPlugin extends OntoWiki_Plugin
 
     public function onDisplayLiteralPropertyValue($event)
     {
-        if (in_array($event->property, $this->properties)) {
+        if (
+            in_array($event->property, $this->properties)
+            || in_array($event->datatype, $this->datatypes)
+        ) {
             require_once 'parser/markdown.php';
 
-            return Markdown($event->value);
+            $mdString = $this->_parseMetadata($event->value);
+
+            return Markdown($mdString);
         }
-        if (in_array($event->datatype, $this->datatypes)) {
-            require_once 'parser/markdown.php';
+    }
 
-            return Markdown($event->value);
+    private function _parseMetadata($mdString)
+    {
+        $metaData = array();
+
+        $lastKey = null;
+        $lineBuffer = '';
+        $strLen = strlen($mdString);
+        for ($i = 0; $i < $strlen; $i++) {
+            $curChar = substr($mdString, $i, 1);
+            if ($curChar != "\n") {
+                $lineBuffer .= $curChar;
+            } else {
+                if (empty(trim($lineBuffer))) {
+                    // end of metadata
+                    break;
+                } else if (substr($lineBuffer, 0, 1) == ' ' && $lastKey !== null) {
+                    // merge to previous line
+                    $metaData[$lastKey] .= PHP_EOL . trim($lineBuffer);
+                    if (substr($lineBuffer, -2) != '  ') {
+                        $lastKey = null;
+                    }
+                    $lineBuffer = '';
+                } else {
+                    $colon = strpos($lineBuffer, ':');
+                    if ($colon !== false) {
+                        $key = trim(substr($lineBuffer, 0, $colon));
+                        $value = trim(substr($lineBuffer, $colon + 1));
+                        $metaData[$key] = trim($value);
+                        if (substr($lineBuffer, -2) == '  ') {
+                            $lastKey = $key;
+                        } else {
+                            $lastKey = null;
+                        }
+                        $lineBuffer = '';
+                    } else {
+                        // This is no metadata
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (count($metaData) == 0) {
+            return $mdString;
+        } else {
+            return substr($mdString, $i + 1);
         }
     }
 }
