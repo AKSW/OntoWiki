a1bc1fdbc003a48aad5dfb89f9bec70f407805c7
Tim Ermilov <yamalight@gmail.com>
2014-05-30 13:00:01 +0200

    rewrite modellist to use sparql query instead of erfert api

--- extensions/modellist/ModellistController.php
+++ extensions/modellist/ModellistController.php
@@ -22,15 +22,9 @@ class ModellistController extends OntoWiki_Controller_Component
     public function init()
     {
         parent::init();
-        $this->session = new Zend_Session_Namespace(_OWSESSION);
-
-        $store = $this->_owApp->erfurt->getStore();
-
-        if (isset($this->session->showHiddenGraphs) && $this->session->showHiddenGraphs == true) {
-            $this->graphUris = $store->getAvailableModels(true);
-        } else {
-            $this->graphUris = $store->getAvailableModels(false);
-        }
+        $sessionKey = 'Modellist' . (isset($config->session->identifier) ? $config->session->identifier : '');
+        $this->stateSession = new Zend_Session_Namespace($sessionKey);
+        $this->_store = $this->_owApp->erfurt->getStore();
     }
 
     /*
@@ -43,6 +37,11 @@ class ModellistController extends OntoWiki_Controller_Component
         // translate navigation title to selected language
         $this->view->placeholder('main.window.title')->set($this->_owApp->translate->_('Model list'));
         $setup = $this->_request->setup;
+        $setupMd5 = md5(json_encode($setup));
+
+        if($this->stateSession->setupMd5 == $setupMd5) {
+            return $this->view->prerendered = $this->stateSession->view;
+        }
 
         // get offset
         $offset = !empty($setup['offset']) ? $setup['offset'] : 0;
@@ -62,7 +61,20 @@ class ModellistController extends OntoWiki_Controller_Component
             $useGraphUriAsLink = true;
         }
 
-        foreach ($this->graphUris as $graphUri => $true) {
+        // TODO: handle hidden models
+        //if (isset($this->session->showHiddenGraphs) && $this->session->showHiddenGraphs == true) {
+        //} else {
+        //}
+        $queryString = "SELECT DISTINCT ?g WHERE { GRAPH ?g { ?s ?p ?o } } ORDER BY ?g OFFSET ".$offset." LIMIT ".($limit+1);
+        $query = Erfurt_Sparql_SimpleQuery::initWithString($queryString);
+        // get extended results
+        $allResults = $this->_store->sparqlQuery($query);
+        $graphUris = array();
+        foreach($allResults as $index => $obj) {
+            $graphUris[] = $obj["g"];
+        }
+
+        foreach ($graphUris as $graphUri) {
             $linkUrl = $this->_config->urlBase . 'model/select/?m=' . urlencode($graphUri);
             if ($useGraphUriAsLink) {
                 if (isset($this->_config->vhosts)) {
@@ -85,23 +97,16 @@ class ModellistController extends OntoWiki_Controller_Component
             // use URI if no title exists
             $label = $graphUri;
             $temp['label'] = $label;
-
-            $temp['backendName'] = $true;
             $models[] = $temp;
-
-            // check if we're full and break
-            if(count($models) > ($limit + $offset)) {
-                break;
-            }
         }
 
         // build initial view
         $this->view->entries = $models;
 
         // set view variable for the show more button
-        $entriesCount = count($this->view->entries) - $offset;
+        $entriesCount = count($this->view->entries);
         if (($entriesCount > $limit)) {
-            $output = array_slice($this->view->entries, 0, $offset + $limit);
+            $output = array_slice($this->view->entries, 0, $limit);
 
             // return only $_limit entries
             $this->view->entries    = $output;
@@ -123,6 +128,17 @@ class ModellistController extends OntoWiki_Controller_Component
             }
         }
 
+        // prepend old list if needed
+        $needPrepend = $offset > 0;
+        if($needPrepend) {
+            $storedGraphs = json_decode($this->stateSession->graphUris);
+            $entries = array();
+            foreach($storedGraphs as $entry) {
+                $entries[] = get_object_vars($entry);
+            }
+            $this->view->entries = array_merge($entries, $this->view->entries);
+        }
+
         // save state to session
         $this->savestateServer($this->view, $setup);
     }
@@ -133,17 +149,22 @@ class ModellistController extends OntoWiki_Controller_Component
     protected function savestateServer($view, $setup)
     {
         // encode setup to json
+        $offset = !empty($setup['offset']) ? $setup['offset'] : 0;
+        $limit = !empty($setup['limit']) ? $setup['limit'] : 10;
         $setup = json_encode($setup);
+        $setupMd5 = md5($setup);
         // replace \' and \" to ' and "
         $replaceFrom = array("\\'", '\\"');
         $replaceTo   = array("'", '"');
         $setup       = str_replace($replaceFrom, $replaceTo, $setup);
 
         // save view, setup and current model to state
-        $sessionKey         = 'Modellist' . (isset($config->session->identifier) ? $config->session->identifier : '');
-        $this->stateSession = new Zend_Session_Namespace($sessionKey);
         $this->stateSession->view  = $view->render("explore.phtml");
         $this->stateSession->setup = $setup;
+        $this->stateSession->setupMd5 = $setupMd5;
+        $this->stateSession->limit = $limit;
+        $this->stateSession->offset = $offset;
         $this->stateSession->model = (string)$this->_owApp->selectedModel;
+        $this->stateSession->graphUris = json_encode($view->entries);
     }
 }
