62e80fb2c40447bb46fb1bb1aed8a79c7bbd6554
Norman Radtke <norman.radtke@gmail.com>
2014-11-17 15:52:46 +0100

    Remove 'from' clause to find all order statements

--- extensions/sortproperties/SortpropertiesPlugin.php
+++ extensions/sortproperties/SortpropertiesPlugin.php
@@ -26,7 +26,6 @@ class SortpropertiesPlugin extends OntoWiki_Plugin
             foreach ($data as $graphUri => $predicates) {
                 $query = new Erfurt_Sparql_SimpleQuery();
                 $query->setProloguePart('SELECT DISTINCT *')
-                    ->addFrom((string)$graphUri)
                     ->setWherePart('WHERE { ?p <' . $this->_privateConfig->sort->property . '> ?o . }');
 
                 $result = $store->sparqlQuery($query);
